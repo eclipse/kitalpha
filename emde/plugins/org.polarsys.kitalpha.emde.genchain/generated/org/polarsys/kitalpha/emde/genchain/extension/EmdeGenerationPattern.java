ee51daaa8f3d8e47d0fd430a1a7c99dbc77f0c52
Thomas Guiu <thomas.guiu@soyatec.com>
2014-12-03 12:11:22 +0100

    fix library names
    Change-Id: I838909c52709a06e7c0bf1370b5e90701de4e8a2
    Signed-off-by: Thomas Guiu <thomas.guiu@soyatec.com>

---
+++ emde/plugins/org.polarsys.kitalpha.emde.genchain/generated/org/polarsys/kitalpha/emde/genchain/extension/EmdeGenerationPattern.java
@@ -0,0 +1,187 @@
+//Generated with EGF 1.2.0.v20140805-0858
+package org.polarsys.kitalpha.emde.genchain.extension;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.egf.model.pattern.Node;
+import org.eclipse.egf.model.domain.Domain;
+import org.eclipse.egf.model.domain.DomainFactory;
+import org.eclipse.egf.model.domain.EMFDomain;
+import org.eclipse.egf.model.domain.DomainViewpoint;
+import org.eclipse.egf.model.domain.TypeDomain;
+import org.eclipse.egf.model.fcore.Activity;
+import org.eclipse.egf.model.fcore.FactoryComponent;
+import org.eclipse.egf.model.fcore.OrchestrationParameter;
+import org.eclipse.egf.model.fprod.ProductionPlan;
+import org.eclipse.egf.model.pattern.PatternContext;
+import org.eclipse.egf.model.types.Type;
+import org.eclipse.egf.pattern.execution.*;
+import org.eclipse.egf.pattern.query.IQuery;
+import org.eclipse.egf.portfolio.genchain.generationChain.EmfGeneration;
+import org.eclipse.egf.portfolio.genchain.generationChain.GenerationElement;
+import org.eclipse.egf.portfolio.genchain.tools.FcoreBuilderConstants;
+import org.eclipse.egf.portfolio.genchain.tools.utils.ActivityInvocationHelper;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+
+public class EmdeGenerationPattern {
+
+	public EmdeGenerationPattern() {
+		//Here is the constructor
+		// add initialisation of the pattern variables (declaration has been already done).
+		this.modelActivity = "platform:/plugin/org.polarsys.kitalpha.emde.portfolio.emf.egf/egf/emf.gen.model.fcore#_fpAN4VhCEd-5FJ833LkcRA";
+		this.editActivity = "platform:/plugin/org.polarsys.kitalpha.emde.portfolio.emf.egf/egf/emf.gen.edit.fcore#__rEKAFjjEd-5FJ833LkcRA";
+		this.editorActivity = "platform:/plugin/org.polarsys.kitalpha.emde.portfolio.emf.egf/egf/emf.gen.editor.fcore#_AItvUFjkEd-5FJ833LkcRA";
+		this.testActivity = "platform:/plugin/org.polarsys.kitalpha.emde.portfolio.emf.egf/egf/emf.gen.tests.fcore#_79dGwDYdEeGWF-mrFIsVzA";
+		this.javadocActivity = "platform:/plugin/org.polarsys.kitalpha.emde.portfolio.emf.egf/egf/javadoc.fcore#_CycMIDYfEeGWF-mrFIsVzA";
+	}
+
+	public void generate(Object argument) throws Exception {
+		InternalPatternContext ctx = (InternalPatternContext) argument;
+		IQuery.ParameterDescription paramDesc = null;
+		Map<String, String> queryCtx = null;
+		Node.Container currentNode = ctx.getNode();
+		List<Object> parameterList = null;
+		//this pattern can only be called by another (i.e. it's not an entry point in execution)
+
+		for (Object parameterParameter : parameterList) {
+
+			this.parameter = (org.polarsys.kitalpha.emde.genchain.extension.model.EmdeGeneration) parameterParameter;
+
+			if (preCondition(ctx)) {
+				ctx.setNode(new Node.Container(currentNode, getClass()));
+				orchestration((PatternContext) argument);
+
+			}
+		}
+		if (ctx.useReporter()) {
+			ctx.getReporter().executionFinished(OutputManager.computeExecutionOutput(ctx), ctx);
+		}
+	}
+
+	public String orchestration(PatternContext ctx) throws Exception {
+		InternalPatternContext ictx = (InternalPatternContext) ctx;
+		Node.Container currentNode = ictx.getNode();
+		method_body(new StringBuffer(), ictx);
+		ictx.setNode(currentNode);
+		if (ictx.useReporter()) {
+			Map<String, Object> parameterValues = new HashMap<String, Object>();
+			parameterValues.put("parameter", this.parameter);
+			String outputWithCallBack = OutputManager.computeLoopOutput(ictx);
+			String loop = OutputManager.computeLoopOutputWithoutCallback(ictx);
+			ictx.getReporter().loopFinished(loop, outputWithCallBack, ictx, parameterValues);
+		}
+		return null;
+	}
+
+	protected void method_body(final StringBuffer out, final PatternContext ctx) throws Exception {
+		Map<GenerationElement, FactoryComponent> fcs = (Map<GenerationElement, FactoryComponent>) ctx.getValue(FcoreBuilderConstants.CURRENT_FCORE);
+
+		FactoryComponent fc = fcs.get((GenerationElement) (parameter.eContainer()));
+
+		ProductionPlan pp = (ProductionPlan) fc.getOrchestration();
+		DomainViewpoint dvp = (DomainViewpoint) fc.getViewpointContainer().getViewpoint(DomainViewpoint.class);
+
+		EMFDomain genModelDomain = null;
+		URI uri = ((HashMap<String, URI>) ctx.getValue(FcoreBuilderConstants.GENMODEL_URIS)).get(parameter.getModelPath());
+		genModelDomain = ActivityInvocationHelper.getDomain(dvp, uri);
+		if (genModelDomain == null) {
+			genModelDomain = DomainFactory.eINSTANCE.createEMFDomain();
+			genModelDomain.setUri(uri);
+			dvp.getDomains().add(genModelDomain);
+		}
+
+		Map<String, Type> contracts = new HashMap<String, Type>();
+		Map<String, OrchestrationParameter> parameters = new HashMap<String, OrchestrationParameter>();
+
+		for (OrchestrationParameter param : pp.getOrchestrationParameters()) {
+			if (ActivityInvocationHelper.GENERATION_EXTENSION_PARAMETER_NAME.equals(param.getName())) {
+				parameters.put("pattern.substitutions", param);
+			}
+		}
+
+		ResourceSet resourceSet = fc.eResource().getResourceSet();
+		if (parameter.isGenerateModel()) {
+			TypeDomain typeDomain = DomainFactory.eINSTANCE.createTypeDomain();
+			typeDomain.setDomain(genModelDomain);
+			contracts.put("genModelURI", typeDomain);
+			ActivityInvocationHelper.addInvocation(pp, (Activity) resourceSet.getEObject(URI.createURI(this.modelActivity, false), true), contracts, parameters);
+		}
+		if (parameter.isGenerateEdit()) {
+			TypeDomain typeDomain = DomainFactory.eINSTANCE.createTypeDomain();
+			typeDomain.setDomain(genModelDomain);
+			contracts.put("genModelURI", typeDomain);
+			ActivityInvocationHelper.addInvocation(pp, (Activity) resourceSet.getEObject(URI.createURI(this.editActivity, false), true), contracts, parameters);
+		}
+		if (parameter.isGenerateEditor()) {
+			TypeDomain typeDomain = DomainFactory.eINSTANCE.createTypeDomain();
+			typeDomain.setDomain(genModelDomain);
+			contracts.put("genModelURI", typeDomain);
+			ActivityInvocationHelper.addInvocation(pp, (Activity) resourceSet.getEObject(URI.createURI(this.editorActivity, false), true), contracts, parameters);
+		}
+		if (parameter.isGenerateTest()) {
+			TypeDomain typeEMFDomain = DomainFactory.eINSTANCE.createTypeDomain();
+			typeEMFDomain.setDomain(genModelDomain);
+			contracts.put("genModelURI", typeEMFDomain);
+			ActivityInvocationHelper.addInvocation(pp, (Activity) resourceSet.getEObject(URI.createURI(this.testActivity, false), true), contracts, parameters);
+		}
+		if (parameter.isGenerateJavadoc()) {
+			TypeDomain typeEMFDomain = DomainFactory.eINSTANCE.createTypeDomain();
+			typeEMFDomain.setDomain(genModelDomain);
+			contracts.put("genModelURI", typeEMFDomain);
+			ActivityInvocationHelper.addInvocation(pp, (Activity) resourceSet.getEObject(URI.createURI(this.javadocActivity, false), true), contracts, parameters);
+		}
+
+		InternalPatternContext ictx = (InternalPatternContext) ctx;
+		new Node.DataLeaf(ictx.getNode(), getClass(), "body", out.toString());
+	}
+
+	public boolean preCondition(PatternContext ctx) throws Exception {
+		return true;
+	}
+
+	protected org.polarsys.kitalpha.emde.genchain.extension.model.EmdeGeneration parameter;
+
+	public void set_parameter(org.polarsys.kitalpha.emde.genchain.extension.model.EmdeGeneration parameter) {
+		this.parameter = parameter;
+	}
+
+	protected java.lang.String modelActivity;
+
+	public void set_modelActivity(java.lang.String modelActivity) {
+		this.modelActivity = modelActivity;
+	}
+
+	protected java.lang.String editActivity;
+
+	public void set_editActivity(java.lang.String editActivity) {
+		this.editActivity = editActivity;
+	}
+
+	protected java.lang.String editorActivity;
+
+	public void set_editorActivity(java.lang.String editorActivity) {
+		this.editorActivity = editorActivity;
+	}
+
+	protected java.lang.String testActivity;
+
+	public void set_testActivity(java.lang.String testActivity) {
+		this.testActivity = testActivity;
+	}
+
+	protected java.lang.String javadocActivity;
+
+	public void set_javadocActivity(java.lang.String javadocActivity) {
+		this.javadocActivity = javadocActivity;
+	}
+
+	public Map<String, Object> getParameters() {
+		Map<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put("parameter", this.parameter);
+		return parameters;
+	}
+
+}
