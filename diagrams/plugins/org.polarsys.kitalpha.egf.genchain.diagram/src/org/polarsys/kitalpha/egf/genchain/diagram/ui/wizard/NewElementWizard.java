f9ae78cf1826bdbad80e4882231b832104739685
Boubekeur Zendagui <boubekeur.zendagui@obeo.fr>
2015-01-12 15:04:19 +0100

    Update copyright for new file
    Change-Id: I7ccb96942014bbf4a7824b4f9b36d82177a25119
    Signed-off-by: Boubekeur Zendagui <boubekeur.zendagui@obeo.fr>

--- diagrams/plugins/org.polarsys.kitalpha.egf.genchain.diagram/src/org/polarsys/kitalpha/egf/genchain/diagram/ui/wizard/NewElementWizard.java
+++ diagrams/plugins/org.polarsys.kitalpha.egf.genchain.diagram/src/org/polarsys/kitalpha/egf/genchain/diagram/ui/wizard/NewElementWizard.java
@@ -1,15 +1,13 @@
-/**
- *
- *  Copyright (c) 2009-2010 Thales Corporate Services S.A.S.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
- * 
- *  Contributors:
- *      Thales Corporate Services S.A.S - initial API and implementation
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2014 Thales Global Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *   Thales Global Services S.A.S - initial API and implementation
+ ******************************************************************************/
 package org.polarsys.kitalpha.egf.genchain.diagram.ui.wizard;
 
 import java.util.Set;
