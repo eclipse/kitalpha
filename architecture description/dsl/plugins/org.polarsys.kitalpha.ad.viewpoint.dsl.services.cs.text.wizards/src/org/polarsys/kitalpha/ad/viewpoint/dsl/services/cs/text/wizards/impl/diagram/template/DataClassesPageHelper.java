510b6b09af60a6c191db88b9ed5c340f0e685396
Boubekeur Zendagui <boubekeur.zendagui@obeo.fr>
2015-02-18 16:10:17 +0100

    [Diagram Text Acceleration] In diagram creattion wizard, keep only classes that extend other classes  when "create Diagram Extension" is checked
    Change-Id: Iac2eaa9091496bfd59fdd1d5eeb2564d9f6a65f0
    Signed-off-by: Boubekeur Zendagui <boubekeur.zendagui@obeo.fr>

---
+++ architecture description/dsl/plugins/org.polarsys.kitalpha.ad.viewpoint.dsl.services.cs.text.wizards/src/org/polarsys/kitalpha/ad/viewpoint/dsl/services/cs/text/wizards/impl/diagram/template/DataClassesPageHelper.java
@@ -0,0 +1,81 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Thales Global Services S.A.S.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *  Thales Global Services S.A.S - initial API and implementation
+ ******************************************************************************/
+
+package org.polarsys.kitalpha.ad.viewpoint.dsl.services.cs.text.wizards.impl.diagram.template;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ListViewer;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.polarsys.kitalpha.ad.viewpoint.dsl.as.model.vpdesc.Class;
+
+/**
+ * @author Boubekeur Zendagui
+ */
+public final class DataClassesPageHelper {
+
+	/**
+	 * Filter classes and return only the ones thats extend other classes
+	 * @param classes initial {@link Class}es list
+	 * @return {@link Class}es extending other classes
+	 */
+	public static Collection<Class> getEmdeExtensionClasses(Collection<Class> classes){
+		Collection<Class> resulClasses = new ArrayList<Class>();
+		for (Class iClass : classes) 
+		{
+			final EList<EClass> classesToExtens = iClass.getExtends();
+			if (classesToExtens != null && ! classesToExtens.isEmpty())
+			{
+				resulClasses.add(iClass);
+			}
+		}
+		return resulClasses;
+	}
+	
+	/**
+	 * Update the content of a {@link ListViewer}
+	 * @param listViewer {@link ListViewer} to update
+	 * @param classList List of {@link Class}es to display in the {@link ListViewer}
+	 */
+	public static void updateListViewerContent(ListViewer listViewer, Collection<Class> classList){
+		ISelection oldSelection = listViewer.getSelection();
+		listViewer.setInput(classList);
+		listViewer.refresh();
+		int selectionNewIndex = getSelectionNewIndex(listViewer, (StructuredSelection)oldSelection);
+		listViewer.setSelection(new StructuredSelection(listViewer.getElementAt(selectionNewIndex)), true);
+	}
+	
+	/**
+	 * @param listViewer {@link ListViewer} to update it content
+	 * @param oldSelection the {@link ListViewer} selected element before update 
+	 * @return New index of the old selection, 0 otherwise.
+	 */
+	public static int getSelectionNewIndex(ListViewer listViewer, StructuredSelection oldSelection) {
+		if (oldSelection != null)
+		{
+			Class clazz = (Class) oldSelection.getFirstElement();
+			final int size = listViewer.getList().getItems().length;
+			for (int i = 0; i < size; i++) 
+			{
+				Class iClass = (Class) listViewer.getElementAt(i);
+				if (iClass.equals(clazz))
+				{
+					return i;
+				}
+			}
+		}
+		return 0;
+	}
+}
