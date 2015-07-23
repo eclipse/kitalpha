edc7074139eae4cfe521e50e3857acfc64976711
Boubekeur Zendagui <boubekeur.zendagui@obeo.fr>
2015-07-23 10:36:27 +0200

    [407] [EMDE] Contextual Explorer eMDE filter contribution.
    Bug: 407
    Change-Id: I2df182727807d57ad277828896da52bd60d477b5
    Signed-off-by: Boubekeur Zendagui <boubekeur.zendagui@obeo.fr>

---
+++ emde/integration/plugins/org.polarsys.kitalpha.emde.explorer.contextual/src/org/polarsys/kitalpha/emde/explorer/contextual/filter/ExplorerContextualEmdeFilter.java
@@ -0,0 +1,43 @@
+/******************************************************************************
+ * Copyright (c) 2015 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Thales - initial API and implementation
+ *****************************************************************************/
+package org.polarsys.kitalpha.emde.explorer.contextual.filter;
+
+import org.eclipse.amalgam.explorer.contextual.core.filter.ICandidateElementFilter;
+import org.polarsys.kitalpha.emde.extension.ModelExtensionHelper;
+import org.polarsys.kitalpha.emde.extension.ModelExtensionManager;
+
+/**
+ * This class implements a filter that accept Emde Element if their ecore model is active.
+ * @author Boubekeur Zendagui
+ */
+public class ExplorerContextualEmdeFilter implements ICandidateElementFilter {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.amalgam.explorer.contextual.core.filter.ICandidateElementFilter#select(java.lang.Object)
+	 */
+	@Override
+	public boolean select(Object object) {
+		// TODO check if the model containing the object is extensible
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.amalgam.explorer.contextual.core.filter.ICandidateElementFilter#accept(java.lang.Object)
+	 */
+	@Override
+	public boolean accept(Object object) {
+		ModelExtensionManager mgr = ModelExtensionHelper.getInstance();
+		return ! mgr.isExtensionModelDisabled(object);
+	}
+
+}
