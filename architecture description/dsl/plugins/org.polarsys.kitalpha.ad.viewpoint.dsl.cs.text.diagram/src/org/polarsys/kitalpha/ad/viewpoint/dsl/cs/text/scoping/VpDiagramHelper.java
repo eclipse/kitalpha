43864456c07cf19ce462a0e254c2865f00419498
Boubekeur Zendagui <boubekeur.zendagui@obeo.fr>
2015-08-24 17:53:18 +0200

    Improving filtering of scope in diagram.vptext.
    Change-Id: I4c7fe9351c0fde898c545a3f23c2dda7b6d197ff
    Signed-off-by: Boubekeur Zendagui <boubekeur.zendagui@obeo.fr>

--- architecture description/dsl/plugins/org.polarsys.kitalpha.ad.viewpoint.dsl.cs.text.diagram/src/org/polarsys/kitalpha/ad/viewpoint/dsl/cs/text/scoping/VpDiagramHelper.java
+++ architecture description/dsl/plugins/org.polarsys.kitalpha.ad.viewpoint.dsl.cs.text.diagram/src/org/polarsys/kitalpha/ad/viewpoint/dsl/cs/text/scoping/VpDiagramHelper.java
@@ -21,6 +21,7 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.sirius.diagram.description.AbstractNodeMapping;
 import org.eclipse.sirius.diagram.description.ContainerMapping;
 import org.eclipse.sirius.diagram.description.DiagramDescription;
 import org.eclipse.sirius.diagram.description.EdgeMapping;
@@ -642,22 +643,34 @@ public class VpDiagramHelper {
 
 
 	public static boolean isValidContainerMapping(EObject eObjectOrProxy, EObject eObject, EReference reference2) {
+		if (false == eObjectOrProxy instanceof ContainerMapping)
+			return false;
+		else
+		{
+			DiagramDescription diagramDescription = getDiagramDescription((ContainerMapping) eObjectOrProxy);
+			return EcoreUtil.equals(diagramDescription, eObject);
+		}
 
-		DiagramExtension diagramExtension = (DiagramExtension)eObject;
-		DiagramDescription diagDesc = diagramExtension.getExtented_diagram();
-		EList<ContainerMapping> mappings = diagDesc.getAllContainerMappings();
-
-		return mappings.contains(eObjectOrProxy);
+//		DiagramExtension diagramExtension = (DiagramExtension)eObject;
+//		DiagramDescription diagDesc = diagramExtension.getExtented_diagram();
+//		EList<ContainerMapping> mappings = diagDesc.getAllContainerMappings();
+//		return mappings.contains(eObjectOrProxy);
 	}
 
 
 	public static boolean isValidNode(EObject eObjectOrProxy, EObject eObject, EReference reference2) {
-
-		DiagramExtension diagramExtension = (DiagramExtension)eObject;
-		DiagramDescription diagDesc = diagramExtension.getExtented_diagram();
-		EList<NodeMapping> nodes = diagDesc.getAllNodeMappings();
-
-		return nodes.contains(eObjectOrProxy);
+		if (false == eObjectOrProxy instanceof NodeMapping)
+			return false;
+		else
+		{
+			DiagramDescription diagramDescription = getDiagramDescription((NodeMapping) eObjectOrProxy);
+			return EcoreUtil.equals(diagramDescription, eObject);
+		}
+//		DiagramExtension diagramExtension = (DiagramExtension)eObject;
+//		DiagramDescription diagDesc = diagramExtension.getExtented_diagram();
+//		EList<NodeMapping> nodes = diagDesc.getAllNodeMappings();
+//
+//		return nodes.contains(eObjectOrProxy);
 	}
 
 
@@ -668,5 +681,18 @@ public class VpDiagramHelper {
 		EList<EdgeMapping> edges = diagDesc.getAllEdgeMappings();
 
 		return edges.contains(eObjectOrProxy);
+	}
+	
+	private static DiagramDescription getDiagramDescription(AbstractNodeMapping mapping){
+		EObject parent = mapping.eContainer();
+		
+		while (parent != null){
+			if (parent instanceof DiagramDescription)
+				return (DiagramDescription) parent;
+			else
+				parent = parent.eContainer();
+		}
+		
+		return null;
 	}
 }
