29d02aa41647fffd50120c9811d6a095820d342f
Boubekeur Zendagui <boubekeur.zendagui@obeo.fr>
2015-09-15 17:07:20 +0200

    [479][AEx] Allowing extensions of viewpoints Pages and Sections
    The validation rule ignores the model element if it root container is
    not a Viewpoint element .
    
    Bug: 479
    Change-Id: I7a5a9674ad7af42f4735106383530e3614e17ac8
    Signed-off-by: Boubekeur Zendagui <boubekeur.zendagui@obeo.fr>

---
+++ architecture description/dsl/plugins/org.polarsys.kitalpha.ad.viewpoint.dsl.as.activityexplorer.helper/src/org/polarsys/kitalpha/ad/viewpoint/dsl/as/activityexplorer/helper/exception/SelfContainedAspectException.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Thales Global Services S.A.S.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *  Thales Global Services S.A.S - initial API and implementation
+ ******************************************************************************/
+
+package org.polarsys.kitalpha.ad.viewpoint.dsl.as.activityexplorer.helper.exception;
+
+/**
+ * @author Boubekeur Zendagui
+ */
+public class SelfContainedAspectException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+	/**
+	 * Default constructor
+	 */
+	public SelfContainedAspectException() {
+		super();
+	}
+}
