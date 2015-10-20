b100d1ce82370c8ef4c41bba4c73c07c7cd95822
Thomas Guiu <thomas.guiu@soyatec.com>
2015-10-20 17:46:03 +0200

    [585] Add 'ComponentSample' example for Transposer
    Bug: 585 
    Change-Id: Ia3138b40466b3001d6f543bc2aada6d6131c0c97
    Signed-off-by: Thomas Guiu <thomas.guiu@soyatec.com>

---
+++ transposer/examples/plugins/org.polarsys.kitalpha.transposer.componentsample.importer/src/org/polarsys/kitalpha/transposer/componentsample/importer/TransposerComponentSampleExampleInstallerWizard.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Thales Global Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *   Thales Global Services S.A.S - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.kitalpha.transposer.componentsample.importer;
+
+import org.eclipse.egf.common.ui.wizard.AllZipsBundleExampleWizard;
+
+/**
+ * @author THomas Guiu
+ *
+ */
+public class TransposerComponentSampleExampleInstallerWizard extends AllZipsBundleExampleWizard {
+
+	@Override
+	protected String getBundleId() {
+		return "org.polarsys.kitalpha.transposer.componentsample.importer"; //$NON-NLS-1$
+	}
+
+}
