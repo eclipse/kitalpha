e0420aa9cbe2527dce616b9097fdec7cef586bc9
Thomas Guiu <thomas.guiu@soyatec.com>
2016-04-01 15:22:40 +0200

    Add new tests
    Change-Id: I74e4c62cb0781662bcd404499fbf3c57e0a9c7fd
    Signed-off-by: Thomas Guiu <thomas.guiu@soyatec.com>

---
+++ architecture description/core/tests/org.polarsys.kitalpha.ad.viewpoint.tests/tests/org/polarsys/kitalpha/ad/viewpoint/tests/BasicTests.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Thales Global Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *   Thales Global Services S.A.S - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.kitalpha.ad.viewpoint.tests;
+
+
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.osgi.framework.Version;
+import org.polarsys.kitalpha.ad.services.manager.AlreadyInStateException;
+import org.polarsys.kitalpha.ad.services.manager.ViewpointActivationException;
+import org.polarsys.kitalpha.ad.services.manager.ViewpointManager;
+import org.polarsys.kitalpha.ad.services.manager.ViewpointManager.Listener;
+import org.polarsys.kitalpha.ad.services.manager.ViewpointManager.OverallListener;
+import org.polarsys.kitalpha.resourcereuse.helper.ResourceReuse;
+import org.polarsys.kitalpha.resourcereuse.model.Resource;
+
+import junit.framework.TestCase;
+
+
+/**
+ * @author Thomas Guiu
+ * 
+ */
+public class BasicTests extends TestCase {
+
+
+	public void testManager1() throws Exception {
+		ResourceSet set1 = new ResourceSetImpl();  
+		ViewpointManager m1 = ViewpointManager.getInstance(set1);
+		assertNotNull(m1);
+	}
+	
+	public void testManager2() throws Exception {
+		ResourceSet set1 = new ResourceSetImpl();  
+		ResourceSet set2 = new ResourceSetImpl();  
+		ViewpointManager m1 = ViewpointManager.getInstance(set1);
+		ViewpointManager m2 = ViewpointManager.getInstance(set2);
+		assertNotSame(m1, m2);
+	}
+	public void testManager3() throws Exception {
+		ResourceSet set1 = new ResourceSetImpl();  
+		ViewpointManager m1 = ViewpointManager.getInstance(set1);
+		ViewpointManager m2 = ViewpointManager.getInstance(set1);
+		assertEquals(m1, m2);
+	}
+
+	public void testManager4() throws Exception {
+		ResourceSet set1 = new ResourceSetImpl();  
+		ViewpointManager m1 = ViewpointManager.getInstance(set1);
+		try {
+			m1.activate("");
+			fail("expecting an exception");
+		} catch (ViewpointActivationException e) {
+		}
+	}
+
+	public void testManager4_1() throws Exception {
+		ResourceSet set1 = new ResourceSetImpl();  
+		ViewpointManager m1 = ViewpointManager.getInstance(set1);
+		try {
+			m1.desactivate("");
+			fail("expecting an exception");
+		} catch (ViewpointActivationException e) {
+		}
+	}
+
+}
---
+++ architecture description/core/tests/org.polarsys.kitalpha.ad.viewpoint.tests/tests/org/polarsys/kitalpha/ad/viewpoint/tests/BasicTests2.java
@@ -0,0 +1,250 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Thales Global Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *   Thales Global Services S.A.S - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.kitalpha.ad.viewpoint.tests;
+
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.osgi.framework.Version;
+import org.polarsys.kitalpha.ad.metadata.helpers.MetadataHelper;
+import org.polarsys.kitalpha.ad.metadata.helpers.ViewpointMetadata;
+import org.polarsys.kitalpha.ad.services.manager.AlreadyInStateException;
+import org.polarsys.kitalpha.ad.services.manager.ViewpointActivationException;
+import org.polarsys.kitalpha.ad.services.manager.ViewpointManager;
+import org.polarsys.kitalpha.ad.services.manager.ViewpointManager.Listener;
+import org.polarsys.kitalpha.ad.services.manager.ViewpointManager.OverallListener;
+import org.polarsys.kitalpha.resourcereuse.helper.ResourceReuse;
+import org.polarsys.kitalpha.resourcereuse.model.Resource;
+
+import junit.framework.TestCase;
+
+
+/**
+ * @author Thomas Guiu
+ * 
+ */
+public class BasicTests2 extends TestCase {
+
+
+	private ResourceSet set;
+	private ViewpointManager m1;
+	
+	@Override
+	protected void setUp() throws Exception {
+		set = new ResourceSetImpl();  
+		URI uri = URI.createPlatformPluginURI("/org.polarsys.kitalpha.ad.viewpoint.tests/resource/My.componentsample", true);
+		set.getResource(uri, true);
+		m1 = ViewpointManager.getInstance(set);
+		MetadataHelper.getViewpointMetadata(set).initMetadataStorage();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		for (org.eclipse.emf.ecore.resource.Resource r : set.getResources())
+			r.unload();
+		set.getResources().clear();
+	}
+
+	public void testManager5() throws Exception {
+		assertFalse(m1.isActive("org.polarsys.kitalpha.vp.componentsampleperformance"));
+		m1.activate("org.polarsys.kitalpha.vp.componentsampleperformance");
+		assertTrue(m1.isActive("org.polarsys.kitalpha.vp.componentsampleperformance"));
+	}
+
+	public void testManager5_1() throws Exception {
+		try {
+			m1.desactivate("org.polarsys.kitalpha.vp.componentsampleperformance");
+			fail("expecting an exception");
+		} catch (AlreadyInStateException e) {
+		}
+	}
+
+	public void testManager6() throws Exception {
+		m1.activate("org.polarsys.kitalpha.vp.componentsampleperformance");
+		try {
+			m1.activate("org.polarsys.kitalpha.vp.componentsampleperformance");
+			fail("expecting an exception");
+		} catch (AlreadyInStateException e) {
+		}
+	}
+	
+	public void testManager7() throws Exception {
+		assertFalse(m1.isActive("org.polarsys.kitalpha.vp.componentsampleperformance"));
+		m1.activate("org.polarsys.kitalpha.vp.componentsampleperformance");
+		assertTrue(m1.isActive("org.polarsys.kitalpha.vp.componentsampleperformance"));
+		m1.desactivate("org.polarsys.kitalpha.vp.componentsampleperformance");
+		assertFalse(m1.isActive("org.polarsys.kitalpha.vp.componentsampleperformance"));
+		assertFalse(m1.isUsed("org.polarsys.kitalpha.vp.componentsampleperformance"));
+	}
+	
+	public void testManager8() throws Exception {
+		assertFalse(m1.isActive("org.polarsys.kitalpha.vp.componentsampleperformance"));
+		m1.startUse("org.polarsys.kitalpha.vp.componentsampleperformance");
+		assertTrue(m1.isActive("org.polarsys.kitalpha.vp.componentsampleperformance"));
+	}
+	
+	public void testManager9() throws Exception {
+		assertFalse(m1.isActive("org.polarsys.kitalpha.vp.componentsampleperformance"));
+		m1.startUse("org.polarsys.kitalpha.vp.componentsampleperformance");
+		assertTrue(m1.isActive("org.polarsys.kitalpha.vp.componentsampleperformance"));
+		m1.stopUse("org.polarsys.kitalpha.vp.componentsampleperformance");
+		assertFalse(m1.isActive("org.polarsys.kitalpha.vp.componentsampleperformance"));
+		assertFalse(m1.isUsed("org.polarsys.kitalpha.vp.componentsampleperformance"));
+	}
+	
+	public void testManager10() throws Exception {
+		assertFalse(m1.isFiltered("org.polarsys.kitalpha.vp.componentsampleperformance"));
+		m1.activate("org.polarsys.kitalpha.vp.componentsampleperformance");
+		assertFalse(m1.isFiltered("org.polarsys.kitalpha.vp.componentsampleperformance"));
+		m1.filter("org.polarsys.kitalpha.vp.componentsampleperformance", true);
+		assertTrue(m1.isFiltered("org.polarsys.kitalpha.vp.componentsampleperformance"));
+		m1.filter("org.polarsys.kitalpha.vp.componentsampleperformance", false);
+		assertFalse(m1.isFiltered("org.polarsys.kitalpha.vp.componentsampleperformance"));
+	}
+	
+	public void testManager11() throws Exception {
+		org.polarsys.kitalpha.resourcereuse.model.Resource resource = ResourceReuse.createHelper().getResource("org.polarsys.kitalpha.vp.componentsampleperformance");
+		assertNull(ViewpointManager.readVersion(resource));
+	}
+	
+	public void testManager12() throws Exception {
+		org.polarsys.kitalpha.resourcereuse.model.Resource resource = ResourceReuse.createHelper().getResource("org.polarsys.kitalpha.vp.componentsamplequalityassessment");
+		Version version = ViewpointManager.readVersion(resource);
+		assertNotNull(version);
+		assertEquals("1.0.0", version.toString());
+	}
+
+	public void testManager13() throws Exception {
+		org.polarsys.kitalpha.resourcereuse.model.Resource resource = ResourceReuse.createHelper().getResource("org.polarsys.kitalpha.vp.componentsamplequalityassessment");
+		Resource viewpoint = ViewpointManager.getViewpoint("org.polarsys.kitalpha.vp.componentsamplequalityassessment");
+		assertEquals(resource.getId(), viewpoint.getId());
+		assertEquals(resource.getDomain(), viewpoint.getDomain());
+		assertEquals(resource.getPath(), viewpoint.getPath());
+		assertEquals(resource.getProviderSymbolicName(), viewpoint.getProviderSymbolicName());
+	}
+
+	public void testManager14() throws Exception {
+		assertEquals(6, ViewpointManager.getAvailableViewpoints().length);
+	}
+
+	public void testManager15() throws Exception {
+		TestListener listener = new TestListener();
+		m1.addListener(listener);
+		
+		m1.activate("org.polarsys.kitalpha.vp.componentsampleperformance");
+		m1.filter("org.polarsys.kitalpha.vp.componentsampleperformance", true);
+		m1.filter("org.polarsys.kitalpha.vp.componentsampleperformance", false);
+		m1.desactivate("org.polarsys.kitalpha.vp.componentsampleperformance");
+		
+		assertTrue(listener.filtered);
+		assertTrue(listener.desactivated);
+		assertTrue(listener.displayed);
+		assertTrue(listener.activated);
+	}
+
+	public void testManager16() throws Exception {
+		ResourceSet set2 = new ResourceSetImpl();  
+		ViewpointManager m2 = ViewpointManager.getInstance(set2);
+		TestListener listener = new TestListener();
+		m2.addListener(listener);
+		
+		m1.activate("org.polarsys.kitalpha.vp.componentsampleperformance");
+		m1.filter("org.polarsys.kitalpha.vp.componentsampleperformance", true);
+		m1.filter("org.polarsys.kitalpha.vp.componentsampleperformance", false);
+		m1.desactivate("org.polarsys.kitalpha.vp.componentsampleperformance");
+		
+		assertFalse(listener.filtered);
+		assertFalse(listener.desactivated);
+		assertFalse(listener.displayed);
+		assertFalse(listener.activated);
+	}
+
+	public void testManager17() throws Exception {
+		ResourceSet set2 = new ResourceSetImpl();  
+		TestOverallListener overallListener = new TestOverallListener();
+		ViewpointManager m2 = ViewpointManager.getInstance(set2);
+		
+		ViewpointManager.addOverallListener(overallListener);
+		TestListener listener1 = new TestListener();
+		m1.addListener(listener1);
+		TestListener listener2 = new TestListener();
+		m2.addListener(listener2);
+
+
+		m1.activate("org.polarsys.kitalpha.vp.componentsampleperformance");
+		m1.filter("org.polarsys.kitalpha.vp.componentsampleperformance", true);
+		m1.filter("org.polarsys.kitalpha.vp.componentsampleperformance", false);
+		m1.desactivate("org.polarsys.kitalpha.vp.componentsampleperformance");
+		
+		assertTrue(overallListener.filtered);
+		assertTrue(overallListener.desactivated);
+		assertTrue(overallListener.displayed);
+		assertTrue(overallListener.activated);
+
+		assertTrue(listener1.filtered);
+		assertTrue(listener1.desactivated);
+		assertTrue(listener1.displayed);
+		assertTrue(listener1.activated);
+		
+		assertFalse(listener2.filtered);
+		assertFalse(listener2.desactivated);
+		assertFalse(listener2.displayed);
+		assertFalse(listener2.activated);
+
+}
+
+	public class TestOverallListener implements OverallListener {
+		public boolean filtered, displayed, activated, desactivated; 
+		@Override
+		public void hasBeenActivated(Object ctx, Resource vp) {
+			activated = true;
+		}
+
+		@Override
+		public void hasBeenDeactivated(Object ctx, Resource vp) {
+			desactivated = true;
+		}
+
+		@Override
+		public void hasBeenFiltered(Object ctx, Resource vp) {
+			filtered = true;
+		}
+
+		@Override
+		public void hasBeenDisplayed(Object ctx, Resource vp) {
+			displayed = true;
+		}
+		
+	}
+	public class TestListener implements Listener {
+		public boolean filtered, displayed, activated, desactivated; 
+		@Override
+		public void hasBeenFiltered(Resource vp) {
+			filtered = true;
+		}
+		
+		@Override
+		public void hasBeenDisplayed(Resource vp) {
+			displayed = true;
+		}
+		
+		@Override
+		public void hasBeenDeactivated(Resource vp) {
+			desactivated = true;
+		}
+		
+		@Override
+		public void hasBeenActivated(Resource vp) {
+			activated = true;
+		}
+	}
+}
