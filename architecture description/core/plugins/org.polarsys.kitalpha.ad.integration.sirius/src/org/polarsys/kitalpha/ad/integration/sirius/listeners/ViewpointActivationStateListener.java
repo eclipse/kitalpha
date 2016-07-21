70f44872cda0aef76936a7143cbff49de9005a96
Thomas Guiu <thomas.guiu@soyatec.com>
2016-07-21 19:25:40 +0200

    [1071] VP activation/desactivation state not synchronized with .aird
    Bug: 1071
    
    Change-Id: I119ecea3fbe0b1f4890685047265e024a66f53fb
    Signed-off-by: Thomas Guiu <thomas.guiu@soyatec.com>

--- architecture description/core/plugins/org.polarsys.kitalpha.ad.integration.sirius/src/org/polarsys/kitalpha/ad/integration/sirius/listeners/ViewpointActivationStateListener.java
+++ architecture description/core/plugins/org.polarsys.kitalpha.ad.integration.sirius/src/org/polarsys/kitalpha/ad/integration/sirius/listeners/ViewpointActivationStateListener.java
@@ -22,17 +22,20 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.emf.common.command.AbstractCommand;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.transaction.RecordingCommand;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.sirius.business.api.componentization.ViewpointRegistry;
 import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.sirius.business.api.session.SessionManagerListener.Stub;
 import org.eclipse.sirius.ui.business.api.viewpoint.ViewpointSelectionCallback;
 import org.polarsys.kitalpha.ad.common.utils.URIHelper;
 import org.polarsys.kitalpha.ad.integration.sirius.Activator;
+import org.polarsys.kitalpha.ad.integration.sirius.SiriusViewpointManager;
 import org.polarsys.kitalpha.ad.services.manager.ViewpointManager;
 import org.polarsys.kitalpha.ad.viewpoint.coredomain.viewpoint.model.Viewpoint;
 
@@ -44,49 +47,32 @@ import org.polarsys.kitalpha.ad.viewpoint.coredomain.viewpoint.model.Viewpoint;
  */
 public class ViewpointActivationStateListener extends Stub {
 
-	private Map<String, org.eclipse.sirius.viewpoint.description.Viewpoint> getAllSiriusViewpoints() {
-		Map<String, org.eclipse.sirius.viewpoint.description.Viewpoint> vps = new HashMap<String, org.eclipse.sirius.viewpoint.description.Viewpoint>();
-		for (org.eclipse.sirius.viewpoint.description.Viewpoint vp : ViewpointRegistry.getInstance().getViewpoints()) {
-			vps.put(vp.getName(), vp);
-		}
-		return vps;
 
-	}
+	private final class SyncCommand extends RecordingCommand implements AbstractCommand.NonDirtying {
+		private final Set<org.eclipse.sirius.viewpoint.description.Viewpoint> newDeselectedViewpoints;
+		private final IProgressMonitor monitor;
+		private final Session session;
+		private final Set<org.eclipse.sirius.viewpoint.description.Viewpoint> newSelectedViewpoints;
 
-	// walk through AF VP to list Sirius vp who must be activated and those who
-	// must be desactivated
-	private void collectionSiriusViewpoint(ViewpointManager mgr, Set<String> toActivate, Set<String> toDesactivate) {
-		// TODO que faire des dépendences entre VP Sirius ?
-		ResourceSet set = new ResourceSetImpl();
-		// // Add AF Sirius viewpoint, used to filtering in diagrams
-		// toActivate.add("Architecture Framework");
-		try {
-			for (org.polarsys.kitalpha.resourcereuse.model.Resource res : ViewpointManager.getAvailableViewpoints()) {
-				try {
-					URI uri = URIHelper.createURI(res);
-
-					List<String> siriusVps = new ArrayList<String>();
-					Viewpoint vp = (Viewpoint) set.getEObject(uri, true);
-					for (org.eclipse.sirius.viewpoint.description.Viewpoint dvp : SiriusHelper.getViewpoints(vp)) {
-						siriusVps.add(dvp.getName());
-					}
-					if (siriusVps.isEmpty())
-						continue;
-					if (mgr.isUsed(res.getId()) && !mgr.isFiltered(res.getId()))
-						toActivate.addAll(siriusVps);
-					else
-						toDesactivate.addAll(siriusVps);
-				} catch (Exception e) {
-					// we got a screw up model file, we don't care
-				}
-			}
-		} finally {
-			for (Resource r : set.getResources()) {
-				r.unload();
-			}
-			set.getResources().clear();
+		private SyncCommand(TransactionalEditingDomain domain, Set<org.eclipse.sirius.viewpoint.description.Viewpoint> newDeselectedViewpoints, IProgressMonitor monitor, Session session, Set<org.eclipse.sirius.viewpoint.description.Viewpoint> newSelectedViewpoints) {
+			super(domain);
+			this.newDeselectedViewpoints = newDeselectedViewpoints;
+			this.monitor = monitor;
+			this.session = session;
+			this.newSelectedViewpoints = newSelectedViewpoints;
 		}
 
+		@Override
+		protected void doExecute() {
+			ViewpointSelectionCallback viewpointSelectionCallback = new ViewpointSelectionCallback();
+
+			for (org.eclipse.sirius.viewpoint.description.Viewpoint vp : newDeselectedViewpoints) {
+				viewpointSelectionCallback.deselectViewpoint(vp, session, monitor);
+			}
+			for (org.eclipse.sirius.viewpoint.description.Viewpoint vp : newSelectedViewpoints) {
+				viewpointSelectionCallback.selectViewpoint(vp, session, monitor);
+			}
+		}
 	}
 
 	public void notifyAddSession(final Session session) {
@@ -106,8 +92,8 @@ public class ViewpointActivationStateListener extends Stub {
 					final Set<String> toActivate = new HashSet<String>();
 					final Set<String> toDesactivate = new HashSet<String>();
 					ViewpointManager mgr = SiriusHelper.getViewpointManager(session);
-					collectionSiriusViewpoint(mgr, toActivate, toDesactivate);
-					final Map<String, org.eclipse.sirius.viewpoint.description.Viewpoint> allSiriusViewpoints = getAllSiriusViewpoints();
+					SiriusViewpointManager.INSTANCE.collectSiriusViewpoint(mgr, toActivate, toDesactivate);
+					final Map<String, org.eclipse.sirius.viewpoint.description.Viewpoint> allSiriusViewpoints = SiriusViewpointManager.INSTANCE.getAllSiriusViewpoints();
 
 					for (org.eclipse.sirius.viewpoint.description.Viewpoint vp : session.getSelectedViewpoints(false)) {
 						if (toActivate.contains(vp.getName()))
@@ -119,20 +105,7 @@ public class ViewpointActivationStateListener extends Stub {
 						newSelectedViewpoints.add(allSiriusViewpoints.get(name));
 					}
 
-					RecordingCommand command = new RecordingCommand(session.getTransactionalEditingDomain()) {
-
-						@Override
-						protected void doExecute() {
-							ViewpointSelectionCallback viewpointSelectionCallback = new ViewpointSelectionCallback();
-
-							for (org.eclipse.sirius.viewpoint.description.Viewpoint vp : newDeselectedViewpoints) {
-								viewpointSelectionCallback.deselectViewpoint(vp, session, monitor);
-							}
-							for (org.eclipse.sirius.viewpoint.description.Viewpoint vp : newSelectedViewpoints) {
-								viewpointSelectionCallback.selectViewpoint(vp, session, monitor);
-							}
-						}
-					};
+					RecordingCommand command = new SyncCommand(session.getTransactionalEditingDomain(), newDeselectedViewpoints, monitor, session, newSelectedViewpoints);
 					if (!newSelectedViewpoints.isEmpty() || !newDeselectedViewpoints.isEmpty())
 						session.getTransactionalEditingDomain().getCommandStack().execute(command);
 				} catch (Exception e) {
