1afa391e11b37185e03fcbc5150b6cfd7eab2738
Faycal Abka <faycal.abka@altran.com>
2016-09-05 16:07:34 +0200

    [1135] [Detachment] Viewpoint Detachment with Sirius Layer. Reactivate kitalpha menu
    Bug: 1135
    
    Change-Id: Ie711beb1736746dbffe3eb30b97255a5aa323f1e
    Signed-off-by: Faycal Abka <faycal.abka@altran.com>

--- attachment/plugins/org.polarsys.kitalpha.model.attachment.ui/src/org/polarsys/kitalpha/model/attachment/ui/action/ModelAttachmentAction.java
+++ attachment/plugins/org.polarsys.kitalpha.model.attachment.ui/src/org/polarsys/kitalpha/model/attachment/ui/action/ModelAttachmentAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 Thales Global Services S.A.S.
+ * Copyright (c) 2014, 2016 Thales Global Services S.A.S.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -26,8 +26,9 @@ import org.polarsys.kitalpha.model.attachment.ui.wizard.AttachmentWizard;
 
 /**
  * @author Thomas Guiu
- * 
+ * @deprecated use ModelAttachmentCommandHandler
  */
+@Deprecated
 public class ModelAttachmentAction implements IObjectActionDelegate {
 
 	private final List<IFile> files = new ArrayList<IFile>();
