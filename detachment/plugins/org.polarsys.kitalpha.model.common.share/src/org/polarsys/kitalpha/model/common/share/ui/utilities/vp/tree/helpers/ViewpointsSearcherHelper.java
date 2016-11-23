b696a2fea8b4633ee51b23c672653e4ecc193c4f
Faycal Abka <faycal.abka@gmail.com>
2016-11-23 17:37:30 +0100

    [1305][Detachment][UI] Activate viewpoint page when viewpoints exist in the workspace launch exception
    Bug: 1305
    
    Change-Id: I164c447ae662f01299e76f6960b6e6cca8fd52a5
    Signed-off-by: Faycal Abka <faycal.abka@altran.com>
    (cherry picked from commit f48dcf304a7dddc205d472f785f636d8dc049e0e)

--- detachment/plugins/org.polarsys.kitalpha.model.common.share/src/org/polarsys/kitalpha/model/common/share/ui/utilities/vp/tree/helpers/ViewpointsSearcherHelper.java
+++ detachment/plugins/org.polarsys.kitalpha.model.common.share/src/org/polarsys/kitalpha/model/common/share/ui/utilities/vp/tree/helpers/ViewpointsSearcherHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 Thales Global Services S.A.S.
+ * Copyright (c) 2014, 2016 Thales Global Services S.A.S.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -34,8 +34,11 @@ public class ViewpointsSearcherHelper {
 		criteria.getTags().add(VIEWPOINT_TAG);
 	}
 	
+	/**
+	 * @return all viewpoint resource in the target platform
+	 */
 	public static Resource [] getAllViewpoints(){
 		initCriteria();
-		return ResourceReuse.createHelper().getResources(criteria);
+		return ResourceReuse.createPlatformHelper().getResources(criteria);
 	}
 }
