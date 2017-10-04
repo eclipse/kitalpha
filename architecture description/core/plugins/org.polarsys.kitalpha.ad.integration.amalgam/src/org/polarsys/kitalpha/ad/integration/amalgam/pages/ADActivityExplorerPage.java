a6f377e294c1b23ccb2012f997c253197b05545a
Faycal Abka <abka.faycal@gmail.com>
2017-10-04 10:40:22 +0200

    [1769][Viewpoint] Generated Activity Explorer pages of viewpoint are available for all editor
    Bug: 1769
    Change-Id: I8720aed59396adbe54fe30a312f84ae802ae2a59
    Signed-off-by: Faycal Abka <abka.faycal@gmail.com>

--- architecture description/core/plugins/org.polarsys.kitalpha.ad.integration.amalgam/src/org/polarsys/kitalpha/ad/integration/amalgam/pages/ADActivityExplorerPage.java
+++ architecture description/core/plugins/org.polarsys.kitalpha.ad.integration.amalgam/src/org/polarsys/kitalpha/ad/integration/amalgam/pages/ADActivityExplorerPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Thales Global Services S.A.S.
+ * Copyright (c) 2015, 2017 Thales Global Services S.A.S.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -35,12 +35,9 @@ public abstract class ADActivityExplorerPage extends BasicSessionActivityExplore
                        String vpid = getViewpointID();
                        boolean used = instance.isUsed(vpid);
                        boolean filtered = instance.isFiltered(vpid);
-                       return used && !filtered;
+                       return used && !filtered &&  super.isVisible();
                  }
           }
-          // return true so that this method will be called again later when object fields are populated
-          return true;
+          return super.isVisible();
     }
-
-
 }
