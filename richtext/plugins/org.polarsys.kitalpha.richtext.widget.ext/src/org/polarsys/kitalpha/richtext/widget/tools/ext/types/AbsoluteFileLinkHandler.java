5989d21b5f0632aced2931c9658e13988274f657
Ali AKAR <ali.akar82@gmail.com>
2018-02-05 14:10:13 +0100

    [1946] [Richtext]Exception when using an absolute path in a description
    Bug 1946
    
    Change-Id: Ic68ed9ad6082bcc67a09f4d687ea8d4f84570ef5
    Signed-off-by: Ali AKAR <ali.akar82@gmail.com>

--- richtext/plugins/org.polarsys.kitalpha.richtext.widget.ext/src/org/polarsys/kitalpha/richtext/widget/tools/ext/types/AbsoluteFileLinkHandler.java
+++ richtext/plugins/org.polarsys.kitalpha.richtext.widget.ext/src/org/polarsys/kitalpha/richtext/widget/tools/ext/types/AbsoluteFileLinkHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Thales Global Services S.A.S.
+ * Copyright (c) 2017, 2018 Thales Global Services S.A.S.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -78,7 +78,7 @@ public class AbsoluteFileLinkHandler implements LinkHandler {
 
 	@Override
 	public String encode(String url, String urlDisplayName) {
-		return "<a href=\"file:///" + url.replace("\\", "\\\\") + "\">" + StringEscapeUtils.escapeHtml(urlDisplayName).replace("\\", "\\\\") + "</a>"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$;
+	  return "<a href=\"file:///" + url.replace("\\", "\\\\") + "\">" + StringEscapeUtils.escapeHtml(urlDisplayName).replace("\\", "\\\\") + "</a>"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$;
 	}
 
 	@Override
