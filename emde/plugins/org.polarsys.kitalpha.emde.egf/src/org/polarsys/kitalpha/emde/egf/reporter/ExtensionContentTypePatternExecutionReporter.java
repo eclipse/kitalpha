1cba94cd9b78ea430c44292bc8eb4bf897c2bfae
Thomas Guiu <thomas.guiu@soyatec.com>
2018-03-30 17:21:27 +0200

    [2008] [eMDE] Generation extension Child Extender is not consistent
    Bug: 2008
    
    Change-Id: I0c1a41c782a1a0502d4dfd41277a5d6dfc5c7d53
    Signed-off-by: Thomas Guiu <thomas.guiu@soyatec.com>

--- emde/plugins/org.polarsys.kitalpha.emde.egf/src/org/polarsys/kitalpha/emde/egf/reporter/ExtensionContentTypePatternExecutionReporter.java
+++ emde/plugins/org.polarsys.kitalpha.emde.egf/src/org/polarsys/kitalpha/emde/egf/reporter/ExtensionContentTypePatternExecutionReporter.java
@@ -1,15 +1,16 @@
 /*******************************************************************************
- * Copyright (c) 2014 Thales Global Services S.A.S.
+ * Copyright (c) 2014, 2018 Thales Global Services S.A.S.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *  
+ *
  * Contributors:
  *   Thales Global Services S.A.S - initial API and implementation
  *******************************************************************************/
 package org.polarsys.kitalpha.emde.egf.reporter;
 
+import org.eclipse.egf.emf.pattern.base.CodegenGeneratorAdapter;
 import org.eclipse.egf.emf.pattern.base.ContentTypePatternExecutionReporter;
 import org.eclipse.egf.model.pattern.PatternContext;
 import org.eclipse.emf.common.util.URI;
@@ -24,7 +25,7 @@ public class ExtensionContentTypePatternExecutionReporter extends ContentTypePat
 	protected void generateJava(String output, PatternContext context) {
 		String targetPath = (String) context.getValue("targetPath"); //$NON-NLS-1$
 		String packageName = (String) context.getValue("packageName"); //$NON-NLS-1$
-		String className = (String) context.getValue("className"); //$NON-NLS-1$    
+		String className = (String) context.getValue("className"); //$NON-NLS-1$
 		if (targetPath == null) {
 			throw new IllegalStateException("Variable targetPath must be set."); //$NON-NLS-1$
 		}
@@ -39,4 +40,24 @@ public class ExtensionContentTypePatternExecutionReporter extends ContentTypePat
 		generator.generateJava(targetPath, packageName, className, output);
 	}
 
+	@Override
+	protected void generateText(String output, PatternContext context) {
+		String targetPathName = (String) context.getValue("targetPathName"); //$NON-NLS-1$
+		Boolean overwrite = (Boolean) context.getValue("overwrite"); //$NON-NLS-1$
+		String encoding = (String) context.getValue("encoding"); //$NON-NLS-1$
+
+		if (targetPathName == null)
+			throw new IllegalStateException("Variable targetPath must be set."); //$NON-NLS-1$
+		if (overwrite == null)
+			throw new IllegalStateException("Variable overwrite must be set."); //$NON-NLS-1$
+		if (encoding == null)
+			throw new IllegalStateException("Variable encoding must be set."); //$NON-NLS-1$
+
+		new CodegenGeneratorAdapter(genModel) {
+			protected String mergePluginXML(String generated, String oldContents, String newContents) {
+				return newContents;
+			}
+		}.generateText(targetPathName, overwrite, encoding, output);
+
+	}
 }
