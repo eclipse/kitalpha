19cb40652cadfb53329f6a71b8123e1d096827a3
Jeremy Aubry <jaubry@obeo.fr>
2018-05-17 15:46:48 +0200

    [2072] [RichText] NPE when deleting a project with a Richtext editor view opened
    Bug: 2072 
    Change-Id: Ie12e0b9d1d72e76ac072f56e9e0dee7a7fb341e5
    Signed-off-by: Jeremy Aubry <jaubry@obeo.fr>

--- richtext/plugins/org.polarsys.kitalpha.richtext.widget/src/org/polarsys/kitalpha/richtext/widget/editor/MDERichTextEditorInput.java
+++ richtext/plugins/org.polarsys.kitalpha.richtext.widget/src/org/polarsys/kitalpha/richtext/widget/editor/MDERichTextEditorInput.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Thales Global Services S.A.S.
+ * Copyright (c) 2017, 2018 Thales Global Services S.A.S.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -62,7 +62,8 @@ public class MDERichTextEditorInput implements IEditorInput{
 
 	@Override
 	public String getToolTipText() {
-		return MDERichTextToolsHelper.getName(getElement());
+		String elementName = MDERichTextToolsHelper.getName(getElement());
+		return elementName != null ? elementName : "";
 	}
 	
 	public EObject getElement() {
