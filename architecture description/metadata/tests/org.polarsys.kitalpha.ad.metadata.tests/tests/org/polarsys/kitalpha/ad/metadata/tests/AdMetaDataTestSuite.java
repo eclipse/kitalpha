cb7ca94648aba6b26ccc83b0088952746ab46734
Thomas Guiu <thomas.guiu@soyatec.com>
2018-11-28 14:41:58 +0100

    [2285] CCE when cast to Metadata
    bug: 2285 
    
    Change-Id: Ifbd520ae9ccb5e30170d5abbf0861983d1fb1749
    Signed-off-by: Thomas Guiu <thomas.guiu@soyatec.com>

--- architecture description/metadata/tests/org.polarsys.kitalpha.ad.metadata.tests/tests/org/polarsys/kitalpha/ad/metadata/tests/AdMetaDataTestSuite.java
+++ architecture description/metadata/tests/org.polarsys.kitalpha.ad.metadata.tests/tests/org/polarsys/kitalpha/ad/metadata/tests/AdMetaDataTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 Thales Global Services S.A.S.
+ * Copyright (c) 2016, 2018 Thales Global Services S.A.S.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -23,6 +23,7 @@ public class AdMetaDataTestSuite {
 		TestSuite suite = new TestSuite("Metadata Test Suite"); //$NON-NLS-1$
 		suite.addTest(new TestSuite(BasicTests.class));
 		suite.addTest(new TestSuite(BasicTests2.class));
+		suite.addTest(new TestSuite(BasicTests3.class));
 		suite.addTest(new TestSuite(ComponentSampleTests2.class));
 		suite.addTest(new TestSuite(ComponentSampleTests.class));
 		suite.addTest(new TestSuite(ModelTests1.class));
