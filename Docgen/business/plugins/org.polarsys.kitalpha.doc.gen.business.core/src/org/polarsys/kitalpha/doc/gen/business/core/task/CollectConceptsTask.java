3b88e141b9388732847c8fab40256e9e9ae340f2
Faycal Abka <abka.faycal@gmail.com>
2019-02-19 15:13:18 +0100

    [2356][GenDoc] Add mechanism to enhance the ergonomy of search index
    - Add missing elements (with same name) in the index
    
    - https://git.polarsys.org/r/#/c/7289/: Abandoned
    
    bug: 2356
    Change-Id: Ic17a8ccfe43bea22d9420f8b57d5aecc36c3466f
    Signed-off-by: Faycal Abka <abka.faycal@gmail.com>

--- Docgen/business/plugins/org.polarsys.kitalpha.doc.gen.business.core/src/org/polarsys/kitalpha/doc/gen/business/core/task/CollectConceptsTask.java
+++ Docgen/business/plugins/org.polarsys.kitalpha.doc.gen.business.core/src/org/polarsys/kitalpha/doc/gen/business/core/task/CollectConceptsTask.java
@@ -10,8 +10,13 @@
  ******************************************************************************/
 package org.polarsys.kitalpha.doc.gen.business.core.task;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.egf.core.producer.InvocationException;
@@ -22,24 +27,43 @@ import org.polarsys.kitalpha.doc.gen.business.core.services.IndexerService;
 
 public class CollectConceptsTask implements ITaskProduction {
 
-	@Override
 	public void preExecute(ITaskProductionContext productionContext,
 			IProgressMonitor monitor) throws InvocationException {
 		// No need fot this step
 	}
 
-	@Override
 	public void doExecute(ITaskProductionContext productionContext,
 			IProgressMonitor monitor) throws InvocationException {
 		List<String> concepts = IndexerService.INSTANCE.getSortedElements();
 		Map<String, IndexItem> indexItems = IndexerService.INSTANCE.getElementsToIndexItems();
-
+		
+		/*
+		 * Sort the map
+		 */
+		List<Entry<String, IndexItem>> entryList = new LinkedList<>(indexItems.entrySet());
+		Collections.sort(entryList, new Comparator<Entry<String, IndexItem>>() {
+			
+			@Override
+			public int compare(Entry<String, IndexItem> a, Entry<String, IndexItem> b) {
+				return a.getValue().compareTo(b.getValue());
+			}
+		});
+		
+		Map<String, IndexItem> sortedMap = new LinkedHashMap<>();
+		
+		for (Entry<String, IndexItem> entry : entryList) {
+			sortedMap.put(entry.getKey(), entry.getValue());
+		}
+		
+		/*
+		 * Concepts List not used. To delete
+		 */
 		productionContext.setOutputValue("concepts", concepts);
-		productionContext.setOutputValue("indexItems", indexItems);
+		
+		productionContext.setOutputValue("indexItems", sortedMap);
 
 	}
 
-	@Override
 	public void postExecute(ITaskProductionContext productionContext,
 			IProgressMonitor monitor) throws InvocationException {
 		// No need fot this step
