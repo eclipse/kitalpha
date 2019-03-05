d53a9aa92181b1080a322ccd24c828ab4781fa41
Thomas Guiu <thomas.guiu@soyatec.com>
2019-03-05 16:46:10 +0100

    [2426] Improve detachment robustness
    Bug: 2426
    
    Change-Id: I9c2fa8320aa310bf3532ccdf04625ff2c268b5f4
    Signed-off-by: Thomas Guiu <thomas.guiu@soyatec.com>

--- detachment/plugins/org.polarsys.kitalpha.model.common.share/src/org/polarsys/kitalpha/model/common/share/modelresources/impl/ModelResource.java
+++ detachment/plugins/org.polarsys.kitalpha.model.common.share/src/org/polarsys/kitalpha/model/common/share/modelresources/impl/ModelResource.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2018 Thales Global Services S.A.S.
+ * Copyright (c) 2014, 2019 Thales Global Services S.A.S.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -70,8 +70,10 @@ public class ModelResource implements IModelResource {
 		String resourceUri = getResourceURI().toString();
 		
 		if (eObjectURI.startsWith("http://") || eObjectURI.isEmpty()){
-			//FIXME check if return null
-			eObjectURI = DetachmentHelper.getPlatformResourceURIFromEObject(eObject).toString();
+			URI platformResourceURIFromEObject = DetachmentHelper.getPlatformResourceURIFromEObject(eObject);
+			if (platformResourceURIFromEObject == null)
+				throw new ModelResourceException("Cannot resolve "+eObjectURI);
+			eObjectURI = platformResourceURIFromEObject.toString();
 		}
 		
 		if (!resourceUri.equals(eObjectURI)){
