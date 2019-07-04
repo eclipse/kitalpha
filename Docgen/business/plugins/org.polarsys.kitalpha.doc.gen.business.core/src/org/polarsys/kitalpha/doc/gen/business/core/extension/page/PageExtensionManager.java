d187242dd6896ef5bb74a5ecda0539ea64710022
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2019-07-04 14:01:33 +0200

    [2539] Contributing with new HTML pages for model elements
    - Enhancing getCategoryActivationStatus(String, String). The category
    status is no more dependent on the first extension be can be unknown if
    there are at least two extensions with different status or if the status
    of at least one extension is Unknown
    
    - Add preferences page to handle generation options
    
    Bug: 2539
    Change-Id: Iba4414e14fc68059738c84daba8f1a3981437ec8
    Signed-off-by: Boubekeur Zendagui <boubekeur.zendagui@thalesgroup.com>
    Signed-off-by: Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>

---
+++ Docgen/business/plugins/org.polarsys.kitalpha.doc.gen.business.core/src/org/polarsys/kitalpha/doc/gen/business/core/extension/page/PageExtensionManager.java
@@ -0,0 +1,110 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2019 Thales Global Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *   Thales Global Services S.A.S - initial API and implementation
+ ******************************************************************************/
+
+package org.polarsys.kitalpha.doc.gen.business.core.extension.page;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.emf.common.util.URI;
+import org.polarsys.kitalpha.doc.gen.business.core.extension.page.PageExtensionElement.PageExtensionActivationStatus;
+import org.polarsys.kitalpha.doc.gen.business.core.extension.page.helpers.NamingOptionalityPreferencesHelper;
+
+/**
+ * @author Boubekeur Zendagui
+ */
+public class PageExtensionManager {
+	
+	private static final String DOCGEN_CONTENT_EXTENSION = "org.polarsys.kitalpha.doc.gen.business.core.contentExtension";
+	private static final String DOCGEN_PAGE_EXTENSION_ELEMENT = "pageExtension";
+	private static final String DOCGEN_PAGE_EXTENSION_ELEMENT__NAME = "name";
+	private static final String DOCGEN_PAGE_EXTENSION_ELEMENT__DESCRIPTION = "description";
+	private static final String DOCGEN_PAGE_EXTENSION_ELEMENT__DOMAIN = "domain";
+	private static final String DOCGEN_PAGE_EXTENSION_ELEMENT__CATEGORY = "category";
+	private static final String DOCGEN_PAGE_EXTENSION_ELEMENT__DEFAULT_STATUS = "defaultStatus";
+	
+	private static final String DOCGEN_PAGE_EXTENSION_PATTERN_URI = "URI";
+	private static final String DOCGEN_PAGE_EXTENSION_PATTERN_PAGE = "pagePattern";
+	private static final String DOCGEN_PAGE_EXTENSION_PATTERN_SIDEBAR = "sidebarPattern";
+	
+	/**
+	 * Load available contributions
+	 * @return a collection of {@link PageExtensionElement}. 
+	 */
+	public Collection<PageExtensionElement> loadExtensions(){
+		Collection<PageExtensionElement> result = new LinkedHashSet<PageExtensionElement>();
+		IConfigurationElement[] config = Platform.getExtensionRegistry().getConfigurationElementsFor(DOCGEN_CONTENT_EXTENSION);
+		if (config.length > 0){
+			for (IConfigurationElement elt : config) {
+				if (elt.getName().equalsIgnoreCase(DOCGEN_PAGE_EXTENSION_ELEMENT)){
+					PageExtensionElement pageExtensionElement = createPageExtensionElement(elt);
+					updateActivationStatusFromUserPreferences(pageExtensionElement);
+					// Manage page patterns URIs
+					pageExtensionElement.getPageURI().addAll(collectPatterns(elt, DOCGEN_PAGE_EXTENSION_PATTERN_PAGE));
+					// Manage side bar patterns URIs
+					pageExtensionElement.getSidebarURI().addAll(collectPatterns(elt, DOCGEN_PAGE_EXTENSION_PATTERN_SIDEBAR));
+					result.add(pageExtensionElement);
+				}
+			}
+		}
+		return result;
+	}
+	
+	/**
+	 * Update activation status of the extesion according to user preference
+	 * @param pageExtensionElement
+	 */
+	private void updateActivationStatusFromUserPreferences(PageExtensionElement pageExtensionElement) {
+		
+		String name = NamingOptionalityPreferencesHelper.getKeyOfPageExtensionElement(pageExtensionElement);
+		String customizedBooleanValue = NamingOptionalityPreferencesHelper.getCustomizedStringValue(name);
+		
+		if (!customizedBooleanValue.isEmpty()) {
+			pageExtensionElement.setCurrentActivactionStatus(PageExtensionActivationStatus.getStatus(customizedBooleanValue));			
+		}
+	}
+
+	/**
+	 * Collect pattern URI from configuration element
+	 * @param element a {@link IConfigurationElement}
+	 * @param pageExtensionElement a {@link String} containing the name of the child configuration Element
+	 */
+	private List<URI> collectPatterns(IConfigurationElement element, String childName) {
+		List<URI> patternURIs = new ArrayList<URI>();
+		for (IConfigurationElement child : element.getChildren(childName)) {
+			String fcorePath = child.getAttribute(DOCGEN_PAGE_EXTENSION_PATTERN_URI);
+			URI uri = URI.createURI(fcorePath);
+			patternURIs.add(uri);
+		}
+		return patternURIs;
+	}
+	
+	/**
+	 * Initialize a {@link PageExtensionElement} from the extension point
+	 * @param element {@link IConfigurationElement}
+	 * @return a {@link PageExtensionElement} without pattern URIs
+	 */
+	public PageExtensionElement createPageExtensionElement(IConfigurationElement element){
+		PageExtensionElement peElement = new PageExtensionElement();
+		peElement.setName(element.getAttribute(DOCGEN_PAGE_EXTENSION_ELEMENT__NAME));
+		peElement.setDescription(element.getAttribute(DOCGEN_PAGE_EXTENSION_ELEMENT__DESCRIPTION));
+		peElement.setDomain(element.getAttribute(DOCGEN_PAGE_EXTENSION_ELEMENT__DOMAIN));
+		peElement.setCategory(element.getAttribute(DOCGEN_PAGE_EXTENSION_ELEMENT__CATEGORY));
+		peElement.setDescription(element.getAttribute(DOCGEN_PAGE_EXTENSION_ELEMENT__DESCRIPTION));
+		String deaultActivationStatus = element.getAttribute(DOCGEN_PAGE_EXTENSION_ELEMENT__DEFAULT_STATUS);
+		peElement.setDefaultActivationStatus(PageExtensionActivationStatus.getStatus(deaultActivationStatus));
+		return peElement;
+	}
+}
---
+++ Docgen/business/plugins/org.polarsys.kitalpha.doc.gen.business.core/src/org/polarsys/kitalpha/doc/gen/business/core/task/PaternPageExtensionsTask.java
@@ -0,0 +1,82 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2019 Thales Global Services S.A.S.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *   Thales Global Services S.A.S - initial API and implementation
+ ******************************************************************************/
+
+package org.polarsys.kitalpha.doc.gen.business.core.task;
+
+import java.util.Collection;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.egf.core.producer.InvocationException;
+import org.eclipse.egf.ftask.producer.context.ITaskProductionContext;
+import org.eclipse.egf.ftask.producer.invocation.ITaskProduction;
+import org.eclipse.egf.model.pattern.Pattern;
+import org.eclipse.egf.model.pattern.PatternFactory;
+import org.eclipse.egf.model.pattern.TypePatternList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.polarsys.kitalpha.doc.gen.business.core.extension.page.PageExtensionElement;
+import org.polarsys.kitalpha.doc.gen.business.core.extension.page.PageExtensionRegistry;
+
+/**
+ * @author Boubekeur Zendagui
+ */
+public class PaternPageExtensionsTask implements ITaskProduction {
+
+	private ResourceSet egfResourceSet = new ResourceSetImpl();
+	private static final String CONTRACT_PATTERN_PAGE_EXTENSIONS = "pattern.page.extensions";
+	private static final String CONTRACT_PATTERN_SIDEBAR_EXTENSIONS = "pattern.sidebar.extensions";
+	private static final String CONTRACT_PATTERN_DOMAIN = "domain";
+
+	@Override
+	public void doExecute(ITaskProductionContext productionContext, IProgressMonitor monitor) throws InvocationException {
+		String domain = productionContext.getInputValue(CONTRACT_PATTERN_DOMAIN, String.class);
+		TypePatternList pagePatterns = PatternFactory.eINSTANCE.createTypePatternList();
+		TypePatternList sidebarPatterns = PatternFactory.eINSTANCE.createTypePatternList();
+		if (domain != null && !domain.isEmpty()) {
+			Collection<PageExtensionElement> activeExtensions = PageExtensionRegistry.getInstance().getActiveExtensions(domain);
+			if (! activeExtensions.isEmpty()) {
+				for (PageExtensionElement ext : activeExtensions) {
+					// Manage page patterns
+					for (URI uri : ext.getPageURI()) {
+						EObject eObject = egfResourceSet.getEObject(uri, true);
+						if (eObject != null && eObject instanceof Pattern) {
+							pagePatterns.getElements().add((Pattern)eObject);
+						}
+					}
+					
+					for (URI uri : ext.getSidebarURI()) {
+						EObject eObject = egfResourceSet.getEObject(uri, true);
+						if (eObject != null && eObject instanceof Pattern) {
+							sidebarPatterns.getElements().add((Pattern)eObject);
+						}
+					}
+					
+				}
+			}
+		}
+		productionContext.setOutputValue(CONTRACT_PATTERN_PAGE_EXTENSIONS, pagePatterns);
+		productionContext.setOutputValue(CONTRACT_PATTERN_SIDEBAR_EXTENSIONS, sidebarPatterns);
+	}
+	
+	
+	@Override
+	public void preExecute(ITaskProductionContext productionContext, IProgressMonitor monitor) throws InvocationException {
+		// No need for this method
+	}
+
+	@Override
+	public void postExecute(ITaskProductionContext productionContext, IProgressMonitor monitor) throws InvocationException {
+		// No need for this method
+	}
+
+}
