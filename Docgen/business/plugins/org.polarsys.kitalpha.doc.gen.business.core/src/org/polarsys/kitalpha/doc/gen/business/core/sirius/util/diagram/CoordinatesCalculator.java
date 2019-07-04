2e2d1c9138ba65b388dc7ee69dbf38cb48208720
Boubekeur Zendagui <boubekeur.zendagui@thalesgroup.com>
2019-07-04 14:01:33 +0200

    [2539] Contributing with new HTML pages for model elements
    - Enhancing getCategoryActivationStatus(String, String). The category
    status is no more dependent on the first extension be can be unknown if
    there are at least two extensions with different status or if the status
    of at least one extension is Unknown
    
    - Add preferences page to handle generation options
    
    Bug: 2539
    Change-Id: Iba4414e14fc68059738c84daba8f1a3981437ec8
    Signed-off-by: Boubekeur Zendagui <boubekeur.zendagui@thalesgroup.com>

--- Docgen/business/plugins/org.polarsys.kitalpha.doc.gen.business.core/src/org/polarsys/kitalpha/doc/gen/business/core/sirius/util/diagram/CoordinatesCalculator.java
+++ Docgen/business/plugins/org.polarsys.kitalpha.doc.gen.business.core/src/org/polarsys/kitalpha/doc/gen/business/core/sirius/util/diagram/CoordinatesCalculator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2018 Thales Global Services S.A.S.
+ * Copyright (c) 2014, 2019 Thales Global Services S.A.S.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -291,7 +291,7 @@ public class CoordinatesCalculator {
 		{
 			final EObject nodeElement = view.getElement();
 			
-			if (gep instanceof SiriusNoteEditPart && ((SiriusNoteEditPart)gep).isLinkNote()) {
+			if (gep instanceof SiriusNoteEditPart /*&& ((SiriusNoteEditPart)gep).isLinkNote()*/) {
 				
 				/*
 				 * Handle navigable note
