f570ac06ed64fa0069b6917965ea7cb3d731b0b3
Laurent Fasani <laurent.fasani@obeo.fr>
2019-09-18 17:18:29 +0200

    [2561] Use Message Translator to internationalize the viewpoints selection dialog
    To be able to internationalize Capella, instead of calling directly the
    getLabel method, we must use the MessageTranslator to get the value from
    the localized properties.
    
    Bug: 2561
    Change-Id: I1cda67aac3400932fb98b8605c16c118e5293147
    Signed-off-by: Mélanie Bats <melanie.bats@obeo.fr>
    Signed-off-by: Laurent Fasani <laurent.fasani@obeo.fr>

--- architecture description/core/plugins/org.polarsys.kitalpha.ad.integration.sirius/src/org/polarsys/kitalpha/ad/integration/sirius/dialog/ViewpointsTableLabelProvider.java
+++ architecture description/core/plugins/org.polarsys.kitalpha.ad.integration.sirius/src/org/polarsys/kitalpha/ad/integration/sirius/dialog/ViewpointsTableLabelProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2018 Thales Global Services S.A.S.
+ * Copyright (c) 2014, 2019 Thales Global Services S.A.S.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -24,6 +24,7 @@ import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.sirius.business.api.componentization.ViewpointRegistry;
 import org.eclipse.sirius.business.api.query.IdentifiedElementQuery;
 import org.eclipse.sirius.common.tools.api.util.EqualityHelper;
+import org.eclipse.sirius.common.tools.api.util.MessageTranslator;
 import org.eclipse.sirius.viewpoint.description.Viewpoint;
 import org.eclipse.sirius.viewpoint.provider.SiriusEditPlugin;
 import org.eclipse.swt.SWT;
@@ -39,147 +40,147 @@ import org.polarsys.kitalpha.ad.integration.sirius.SiriusViewpointManager;
  * 
  */
 public class ViewpointsTableLabelProvider extends ColumnLabelProvider {
-	private final Map<Viewpoint, Boolean> viewpoints;
+    private final Map<Viewpoint, Boolean> viewpoints;
 
-	private int columnIndex;
+    private int columnIndex;
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param viewpoints
-	 *            the viewpoints
-	 */
-	public ViewpointsTableLabelProvider(final Map<Viewpoint, Boolean> viewpoints) {
-		super();
-		this.viewpoints = viewpoints;
-	}
+    /**
+     * Constructor.
+     * 
+     * @param viewpoints
+     *            the viewpoints
+     */
+    public ViewpointsTableLabelProvider(final Map<Viewpoint, Boolean> viewpoints) {
+        super();
+        this.viewpoints = viewpoints;
+    }
 
-	private boolean findViewpoint(final Viewpoint vp) {
-		for (final Map.Entry<Viewpoint, Boolean> entry : viewpoints.entrySet()) {
-			if (EqualityHelper.areEquals(entry.getKey(), vp) && entry.getValue()) {
-				return true;
-			}
-		}
-		return false;
-	}
+    private boolean findViewpoint(final Viewpoint vp) {
+        for (final Map.Entry<Viewpoint, Boolean> entry : viewpoints.entrySet()) {
+            if (EqualityHelper.areEquals(entry.getKey(), vp) && entry.getValue()) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	private ImageDescriptor getOverlayedDescriptor(final Image baseImage, final String decoratorPath) {
-		final ImageDescriptor decoratorDescriptor = SiriusEditPlugin.Implementation.getBundledImageDescriptor(decoratorPath);
-		return new DecorationOverlayIcon(baseImage, decoratorDescriptor, IDecoration.BOTTOM_LEFT);
-	}
+    private ImageDescriptor getOverlayedDescriptor(final Image baseImage, final String decoratorPath) {
+        final ImageDescriptor decoratorDescriptor = SiriusEditPlugin.Implementation.getBundledImageDescriptor(decoratorPath);
+        return new DecorationOverlayIcon(baseImage, decoratorDescriptor, IDecoration.BOTTOM_LEFT);
+    }
 
-	private Image getEnhancedImage(final Image image, final Viewpoint viewpoint) {
-		if (ViewpointRegistry.getInstance().isFromPlugin(viewpoint) && image != null) {
-			return SiriusEditPlugin.getPlugin().getImage(getOverlayedDescriptor(image, "icons/full/others/plugin.gif"));
-		}
-		return image;
-	}
+    private Image getEnhancedImage(final Image image, final Viewpoint viewpoint) {
+        if (ViewpointRegistry.getInstance().isFromPlugin(viewpoint) && image != null) {
+            return SiriusEditPlugin.getPlugin().getImage(getOverlayedDescriptor(image, "icons/full/others/plugin.gif"));
+        }
+        return image;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Image getImage(final Object element) {
-		if (!(element instanceof Viewpoint)){
-			return null;
-		}
-		Image image = null;
-		final Viewpoint vp = (Viewpoint) element;
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Image getImage(final Object element) {
+        if (!(element instanceof Viewpoint)) {
+            return null;
+        }
+        Image image = null;
+        final Viewpoint vp = (Viewpoint) element;
 
-		switch (columnIndex) {
-		case 0:
-			boolean underControl = SiriusViewpointManager.INSTANCE.isUnderControl(vp);
-			if (findViewpoint(vp)) {
-				return underControl ? Activator.getDefault().getImage(Activator.IMG_CHECKBOX_GRAYED_SELECTED) : SiriusEditPlugin.getPlugin().getBundledImage("/icons/full/others/checkbox_active.gif");
-			}
-			return underControl ? Activator.getDefault().getImage(Activator.IMG_CHECKBOX_GRAYED_UNSELECTED) : SiriusEditPlugin.getPlugin().getBundledImage("/icons/full/others/checkbox_inactive.gif");
-		case 1:
-			if (vp.getIcon() != null && vp.getIcon().length() > 0) {
-				final ImageDescriptor desc = SiriusEditPlugin.Implementation.findImageDescriptor(vp.getIcon());
-				if (desc != null) {
-					image = SiriusEditPlugin.getPlugin().getImage(desc);
-					return getEnhancedImage(image, vp);
-				}
-			}
-			image = SiriusEditPlugin.getPlugin().getImage(SiriusEditPlugin.getPlugin().getItemImageDescriptor(vp));
-			return getEnhancedImage(image, vp);
-		default:
-			return null;
-		}
-	}
+        switch (columnIndex) {
+        case 0:
+            boolean underControl = SiriusViewpointManager.INSTANCE.isUnderControl(vp);
+            if (findViewpoint(vp)) {
+                return underControl ? Activator.getDefault().getImage(Activator.IMG_CHECKBOX_GRAYED_SELECTED) : SiriusEditPlugin.getPlugin().getBundledImage("/icons/full/others/checkbox_active.gif");
+            }
+            return underControl ? Activator.getDefault().getImage(Activator.IMG_CHECKBOX_GRAYED_UNSELECTED) : SiriusEditPlugin.getPlugin().getBundledImage("/icons/full/others/checkbox_inactive.gif");
+        case 1:
+            if (vp.getIcon() != null && vp.getIcon().length() > 0) {
+                final ImageDescriptor desc = SiriusEditPlugin.Implementation.findImageDescriptor(vp.getIcon());
+                if (desc != null) {
+                    image = SiriusEditPlugin.getPlugin().getImage(desc);
+                    return getEnhancedImage(image, vp);
+                }
+            }
+            image = SiriusEditPlugin.getPlugin().getImage(SiriusEditPlugin.getPlugin().getItemImageDescriptor(vp));
+            return getEnhancedImage(image, vp);
+        default:
+            return null;
+        }
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public String getText(final Object element) {
-		switch (columnIndex) {
-		case 2:
-			if (element instanceof Viewpoint) {
-				return new IdentifiedElementQuery((Viewpoint) element).getLabel();
-			}
-			break;
-		default:
-			break;
-		}
-		return null;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getText(final Object element) {
+        switch (columnIndex) {
+        case 2:
+            if (element instanceof Viewpoint) {
+                return MessageTranslator.INSTANCE.getMessage((Viewpoint) element, new IdentifiedElementQuery((Viewpoint) element).getLabel());
+            }
+            break;
+        default:
+            break;
+        }
+        return null;
+    }
 
-	@Override
-	public String getToolTipText(final Object element) {
-		String toolTip = null;
-		if (columnIndex == 2 && element instanceof Viewpoint) {
-			Viewpoint viewpoint = (Viewpoint) element;
-			final Resource resource = ((Viewpoint) element).eResource();
-			if (resource != null) {
-				toolTip = resource.getURI().toString();
-			}
-			if (viewpoint.getEndUserDocumentation() != null && viewpoint.getEndUserDocumentation().trim().length() > 0) {
-				if (toolTip != null) {
-					toolTip += "\n\n";
-				} else {
-					toolTip = "";
-				}
-				toolTip += viewpoint.getEndUserDocumentation();
-			}
-		}
-		return toolTip;
-	}
+    @Override
+    public String getToolTipText(final Object element) {
+        String toolTip = null;
+        if (columnIndex == 2 && element instanceof Viewpoint) {
+            Viewpoint viewpoint = (Viewpoint) element;
+            final Resource resource = ((Viewpoint) element).eResource();
+            if (resource != null) {
+                toolTip = resource.getURI().toString();
+            }
+            if (viewpoint.getEndUserDocumentation() != null && viewpoint.getEndUserDocumentation().trim().length() > 0) {
+                if (toolTip != null) {
+                    toolTip += "\n\n";
+                } else {
+                    toolTip = "";
+                }
+                toolTip += viewpoint.getEndUserDocumentation();
+            }
+        }
+        return toolTip;
+    }
 
-	@Override
-	public Point getToolTipShift(final Object object) {
-		return new Point(5, 5);
-	}
+    @Override
+    public Point getToolTipShift(final Object object) {
+        return new Point(5, 5);
+    }
 
-	@Override
-	public int getToolTipDisplayDelayTime(final Object object) {
-		return 200;
-	}
+    @Override
+    public int getToolTipDisplayDelayTime(final Object object) {
+        return 200;
+    }
 
-	@Override
-	public Color getBackground(Object element) {
-		return super.getBackground(element);
-	}
+    @Override
+    public Color getBackground(Object element) {
+        return super.getBackground(element);
+    }
 
-	@Override
-	public void update(final ViewerCell cell) {
-		columnIndex = cell.getColumnIndex();
-		Font font = cell.getFont();
-		super.update(cell);
-		Object element = cell.getElement();
-		if (font != null && font.getFontData().length != 0 && element instanceof Viewpoint && SiriusViewpointManager.INSTANCE.isUnderControl((Viewpoint) element)) {
-			String name = font.getFontData()[0].getName();
-			FontRegistry fontRegistry = JFaceResources.getFontRegistry();
-			Font italic = fontRegistry.getItalic(name);
-			cell.setFont(italic);
-		} else {
-			cell.setFont(null);
-		}
-	}
+    @Override
+    public void update(final ViewerCell cell) {
+        columnIndex = cell.getColumnIndex();
+        Font font = cell.getFont();
+        super.update(cell);
+        Object element = cell.getElement();
+        if (font != null && font.getFontData().length != 0 && element instanceof Viewpoint && SiriusViewpointManager.INSTANCE.isUnderControl((Viewpoint) element)) {
+            String name = font.getFontData()[0].getName();
+            FontRegistry fontRegistry = JFaceResources.getFontRegistry();
+            Font italic = fontRegistry.getItalic(name);
+            cell.setFont(italic);
+        } else {
+            cell.setFont(null);
+        }
+    }
 
-	@Override
-	public int getToolTipStyle(final Object object) {
-		return SWT.SHADOW_OUT;
-	}
+    @Override
+    public int getToolTipStyle(final Object object) {
+        return SWT.SHADOW_OUT;
+    }
 
 }
