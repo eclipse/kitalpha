cd03ffceee4e58d97b5a576114ef926118ed7ff6
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2019-11-22 09:14:02 +0100

    [2677] Save output path
    - Restore previously used output path for documentation generation
    
    Bug: 2677
    Change-Id: Ia050b8ee6cb58048d3519ff90e673742ef522f64
    Signed-off-by: Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>

--- Docgen/business/plugins/org.polarsys.kitalpha.doc.gen.business.core.ui/src/org/polarsys/kitalpha/doc/gen/business/core/ui/wizards/HTMLDocumentationGenerationWizard.java
+++ Docgen/business/plugins/org.polarsys.kitalpha.doc.gen.business.core.ui/src/org/polarsys/kitalpha/doc/gen/business/core/ui/wizards/HTMLDocumentationGenerationWizard.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2018 Thales Global Services S.A.S.
+ * Copyright (c) 2014, 2019 Thales Global Services S.A.S.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -164,6 +164,7 @@ public abstract class HTMLDocumentationGenerationWizard extends Wizard
 	@Override
 	public boolean performFinish() {
 		brandingPage.askUserForUpdatingBrandingDataPreferences();
+		page.savePreferences();
 		
 		/** Collect data from wizard pages **/
 		final String modelURI = page.getModelURI();
