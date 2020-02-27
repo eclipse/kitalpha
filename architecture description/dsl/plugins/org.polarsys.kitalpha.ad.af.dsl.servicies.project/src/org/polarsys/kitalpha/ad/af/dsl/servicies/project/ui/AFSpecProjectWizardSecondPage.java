8cedceb28a842ecc87db7cac8800ef5943d9e93b
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2020-02-27 13:30:01 +0100

    [555863] Technical Debt.
    Update copyright years.
    
    Change-Id: I70b4eb2881873141df7343d26a9276bb22f4d1fe
    Signed-off-by: Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>

--- architecture description/dsl/plugins/org.polarsys.kitalpha.ad.af.dsl.servicies.project/src/org/polarsys/kitalpha/ad/af/dsl/servicies/project/ui/AFSpecProjectWizardSecondPage.java
+++ architecture description/dsl/plugins/org.polarsys.kitalpha.ad.af.dsl.servicies.project/src/org/polarsys/kitalpha/ad/af/dsl/servicies/project/ui/AFSpecProjectWizardSecondPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2018 Thales Global Services S.A.S.
+ * Copyright (c) 2014, 2020 Thales Global Services S.A.S.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -410,6 +410,8 @@ public class AFSpecProjectWizardSecondPage extends AbstractAFSpecProjectWizardPa
 			case selectedCreator_source:
 				key = selectedCreator.keySet();
 				break;
+			default:
+				return null;
 			}
 			if (key == null){
 				return null;
