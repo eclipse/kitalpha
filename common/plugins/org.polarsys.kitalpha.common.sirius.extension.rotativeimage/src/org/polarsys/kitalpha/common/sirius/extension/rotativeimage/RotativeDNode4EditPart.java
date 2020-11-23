3f37e516f795d67517b934a16adf02b80de540ba
Thomas Guiu <59727296+tguiu@users.noreply.github.com>
2020-11-23 09:31:42 +0100

    [567287] move rotativeImage to kitalpha core (#69)
    * [567287] move rotativeImage to kitalpha core
    
    includes Capella changes
    
    * move jdk level to 1.8
    
    * add missing changes
    
    * add test

---
+++ common/plugins/org.polarsys.kitalpha.common.sirius.extension.rotativeimage/src/org/polarsys/kitalpha/common/sirius/extension/rotativeimage/RotativeDNode4EditPart.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2020 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.kitalpha.common.sirius.extension.rotativeimage;
+
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.sirius.diagram.ui.internal.edit.parts.DNode4EditPart;
+
+public class RotativeDNode4EditPart extends DNode4EditPart {
+
+	public RotativeDNode4EditPart(View view_p) {
+		super(view_p);
+	}
+
+}
--- common/plugins/org.polarsys.kitalpha.common.sirius.extension.rotativeimage/src/org/polarsys/kitalpha/common/sirius/extension/rotativeimage/RotativeImageEditPart.java
+++ common/plugins/org.polarsys.kitalpha.common.sirius.extension.rotativeimage/src/org/polarsys/kitalpha/common/sirius/extension/rotativeimage/RotativeImageEditPart.java
@@ -42,6 +42,8 @@ import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.sirius.diagram.WorkspaceImage;
 import org.eclipse.sirius.diagram.ui.edit.api.part.IDiagramBorderNodeEditPart;
 import org.eclipse.sirius.diagram.ui.edit.api.part.IStyleEditPart;
+import org.eclipse.sirius.diagram.ui.internal.edit.parts.WorkspaceImageEditPart;
+import org.eclipse.sirius.diagram.ui.tools.api.figure.IWorkspaceImageFigure;
 import org.eclipse.sirius.diagram.ui.tools.api.figure.WorkspaceImageFigure;
 import org.eclipse.sirius.diagram.ui.tools.api.figure.locator.DBorderItemLocator;
 import org.eclipse.sirius.ext.gmf.runtime.gef.ui.figures.AirStyleDefaultSizeNodeFigure;
@@ -52,200 +54,59 @@ import org.polarsys.kitalpha.common.sirius.extension.internal.rotativeimage.Acti
  * 
  * @author ymortier,pdul
  */
-public class RotativeImageEditPart extends ShapeNodeEditPart implements
-		IStyleEditPart {
-
-	/**
-	 * @generated
-	 */
-	public static final int VISUAL_ID = 3005;
+public class RotativeImageEditPart extends WorkspaceImageEditPart implements IStyleEditPart {
 
 	/**
 	 * @generated
 	 */
 	protected IFigure contentPane;
 
-	/**
-	 * @generated
-	 */
-	protected RotativeWorkspaceImageFigure primaryShape;
+	private SwitchImageListener switchImageListener;
 
 	/**
 	 * Creates a new port edit part.
 	 * 
-	 * @param view
-	 *            the GMF view.
+	 * @param view the GMF view.
 	 */
 	public RotativeImageEditPart(View view) {
 		super(view);
-	}
-
-	protected void createDefaultEditPolicies() {
-		// empty.
-	}
-
-	/**
-	 * @generated NOT : prevent drag of elements
-	 */
-	public DragTracker getDragTracker(Request request) {
-		return getParent().getDragTracker(request);
-	}
-
-	/**
-	 * @see org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeEditPart#refreshVisuals()
-	 */
-	protected void refreshVisuals() {
-		super.refreshVisuals();
-		WorkspaceImageFigure figure = this.getPrimaryShape();
-		EObject element = this.resolveSemanticElement();
-		if (element instanceof WorkspaceImage) {
-			WorkspaceImage bundledImage = (WorkspaceImage) element;
-			figure.refreshFigure(bundledImage);
-			if (switchImageListener != null)
-				switchImageListener.updateImage();
-			((GraphicalEditPart) this.getParent()).setLayoutConstraint(this,
-					this.getFigure(), new Rectangle(0, 0, figure
-							.getPreferredSize().width, figure
-							.getPreferredSize().height));
-		}
-	}
-
-	/**
-	 * @generated
-	 */
-	protected LayoutEditPolicy createLayoutEditPolicy() {
-		LayoutEditPolicy lep = new LayoutEditPolicy() {
-
-			protected EditPolicy createChildEditPolicy(EditPart child) {
-				EditPolicy result = child
-						.getEditPolicy(EditPolicy.PRIMARY_DRAG_ROLE);
-				if (result == null) {
-					result = new NonResizableEditPolicy();
-				}
-				return result;
-			}
-
-			protected Command getMoveChildrenCommand(Request request) {
-				return null;
-			}
-
-			protected Command getCreateCommand(CreateRequest request) {
-				return null;
-			}
-		};
-		return lep;
 	}
 
 	/**
 	 * @generated NOT
 	 */
 	protected IFigure createNodeShape() {
-		WorkspaceImage wkImage = (WorkspaceImage) resolveSemanticElement();
-
-		RotativeDescription desc = Activator.getDefault().getBestDescription(
-				wkImage.getWorkspacePath());
-
-		if (desc.mode == RotativeDescription.ROTATIVE) {
-			primaryShape = new RotativeWorkspaceImageFigure(wkImage
-					.getWorkspacePath());
-
-		} else {
-			int pos = desc.id.lastIndexOf(".");
-			String deb = desc.id.substring(0, pos);
-			String ext = desc.id.substring(pos);
-
-			primaryShape = new RotativeWorkspaceImageFigure(desc.id,
-					WorkspaceImageFigure.flyWeightImage(deb + "_top" + ext),
-					WorkspaceImageFigure.flyWeightImage(deb + "_left" + ext),
-					WorkspaceImageFigure.flyWeightImage(deb + "_bottom" + ext),
-					WorkspaceImageFigure.flyWeightImage(deb + "_right" + ext));
+		EObject element = resolveSemanticElement();
+		if (element instanceof WorkspaceImage) {
+			WorkspaceImage wkImage = (WorkspaceImage) element;
+	
+			RotativeDescription desc = Activator.getDefault().getBestDescription(wkImage.getWorkspacePath());
+	
+			if (desc.mode == RotativeDescription.ROTATIVE) {
+				primaryShape = new RotativeWorkspaceImageFigure(wkImage.getWorkspacePath());
+	
+			} else {
+				int pos = desc.id.lastIndexOf(".");
+				String deb = desc.id.substring(0, pos);
+				String ext = desc.id.substring(pos);
+	
+				RotativeWorkspaceImageHelper.createImage(desc.id, deb + "_top" + ext, PositionConstants.NORTH);
+				RotativeWorkspaceImageHelper.createImage(desc.id, deb + "_left" + ext, PositionConstants.WEST);
+				RotativeWorkspaceImageHelper.createImage(desc.id, deb + "_bottom" + ext, PositionConstants.SOUTH);
+				RotativeWorkspaceImageHelper.createImage(desc.id, deb + "_right" + ext, PositionConstants.EAST);
+				primaryShape = new RotativeWorkspaceImageFigure(desc.id);
+			}
+	
+			switchImageListener = new SwitchImageListener(this);
+			primaryShape.addAncestorListener(switchImageListener);
+			primaryShape.addPropertyChangeListener(switchImageListener);
 		}
-
-		switchImageListener = new SwitchImageListener(this);
-		primaryShape.addAncestorListener(switchImageListener);
-		primaryShape.addPropertyChangeListener(switchImageListener);
 		return primaryShape;
 	}
 
-	SwitchImageListener switchImageListener;
-
-	/**
-	 * @generated NOT
-	 */
-	public WorkspaceImageFigure getPrimaryShape() {
-		return (WorkspaceImageFigure) primaryShape;
-	}
-
-	/**
-	 * @not-generated custom size node.
-	 */
-	protected NodeFigure createNodePlate() {
-		DefaultSizeNodeFigure result = new AirStyleDefaultSizeNodeFigure(
-				getMapMode().DPtoLP(40), getMapMode().DPtoLP(40));
-		return result;
-	}
-
-	/**
-	 * @generated
-	 */
-	public EditPolicy getPrimaryDragEditPolicy() {
-		EditPolicy result = super.getPrimaryDragEditPolicy();
-		if (result instanceof ResizableEditPolicy) {
-			ResizableEditPolicy ep = (ResizableEditPolicy) result;
-			ep.setResizeDirections(PositionConstants.NONE);
-		}
-		return result;
-	}
-
-	/**
-	 * Creates figure for this edit part.
-	 * 
-	 * Body of this method does not depend on settings in generation model so
-	 * you may safely remove <i>generated</i> tag and modify it.
-	 * 
-	 * @not-generated
-	 */
-	protected NodeFigure createNodeFigure() {
-		NodeFigure figure = createNodePlate();
-		figure.setLayoutManager(new XYLayout());
-		IFigure shape = createNodeShape();
-		figure.add(shape);
-		contentPane = setupContentPane(shape);
-		return figure;
-	}
-
-	/**
-	 * Default implementation treats passed figure as content pane. Respects
-	 * layout one may have set for generated figure.
-	 * 
-	 * @param nodeShape
-	 *            instance of generated figure class
-	 * @generated
-	 */
-	protected IFigure setupContentPane(IFigure nodeShape) {
-		return nodeShape; // use nodeShape itself as contentPane
-	}
-
-	/**
-	 * @generated
-	 */
-	public IFigure getContentPane() {
-		if (contentPane != null) {
-			return contentPane;
-		}
-		return super.getContentPane();
-	}
-
-	protected Class<?> getMetamodelType() {
-		return WorkspaceImage.class;
-	}
-
-	private static class SwitchImageListener implements AncestorListener,
-			PropertyChangeListener, FigureListener {
+	private static class SwitchImageListener implements AncestorListener, PropertyChangeListener, FigureListener {
 
 		private RotativeImageEditPart editPart;
-
-		boolean update = false;
 
 		public SwitchImageListener(final RotativeImageEditPart editPart) {
 			this.editPart = editPart;
@@ -265,36 +126,16 @@ public class RotativeImageEditPart extends ShapeNodeEditPart implements
 		}
 
 		public void updateImage() {
-			if (editPart.figure == null)
+			if (editPart.figure == null || editPart.getPrimaryShape() == null)
 				return;
-			update = true;
 			IBorderItemLocator borderItemLocator = getBorderItemLocator();
 			if (borderItemLocator != null) {
 				BorderedNodeFigure borderedNodeFigure = getBorderedNodeFigure();
-				int side = DBorderItemLocator.findClosestSideOfParent(editPart
-						.getFigure().getBounds(), borderedNodeFigure
-						.getBounds());
+				int side = DBorderItemLocator.findClosestSideOfParent(editPart.getFigure().getBounds(),	borderedNodeFigure.getBounds());
 
-				switch (side) {
-				case PositionConstants.SOUTH:
-					editPart.primaryShape.setImage(editPart.primaryShape
-							.getBottomImage());
-					break;
-				case PositionConstants.NORTH:
-					editPart.primaryShape.setImage(editPart.primaryShape
-							.getTopImage());
-					break;
-				case PositionConstants.WEST:
-					editPart.primaryShape.setImage(editPart.primaryShape
-							.getLeftImage());
-					break;
-				case PositionConstants.EAST:
-					editPart.primaryShape.setImage(editPart.primaryShape
-							.getRightImage());
-					break;
-				}
+				RotativeWorkspaceImageFigure primaryShape = (RotativeWorkspaceImageFigure)editPart.primaryShape;
+				primaryShape.setOrientation(side);
 			}
-			update = false;
 		}
 
 		private BorderedNodeFigure getBorderedNodeFigure() {
@@ -312,11 +153,7 @@ public class RotativeImageEditPart extends ShapeNodeEditPart implements
 				current = (IGraphicalEditPart) current.getParent();
 			}
 
-			if (borderNodeEditPart instanceof IBorderedShapeEditPart) {
-				borderedNodeFigure = ((IBorderedShapeEditPart) borderNodeEditPart)
-						.getBorderedFigure();
-			}
-
+			borderedNodeFigure = borderNodeEditPart != null ? borderNodeEditPart.getBorderedFigure() : null;
 			return borderedNodeFigure;
 		}
 
@@ -332,8 +169,7 @@ public class RotativeImageEditPart extends ShapeNodeEditPart implements
 			}
 
 			if (borderNodeEditPart instanceof IBorderItemEditPart) {
-				borderItemLocator = ((IBorderItemEditPart) borderNodeEditPart)
-						.getBorderItemLocator();
+				borderItemLocator = ((IBorderItemEditPart) borderNodeEditPart).getBorderItemLocator();
 			}
 
 			return borderItemLocator;
