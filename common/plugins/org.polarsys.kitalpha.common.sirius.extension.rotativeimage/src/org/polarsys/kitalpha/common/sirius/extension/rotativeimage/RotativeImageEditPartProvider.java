535c203b1d1dea70ec812e3e16d2175ba18a0712
fabka4t3 <75066126+fabka4t3@users.noreply.github.com>
2021-04-02 16:54:48 +0200

    #656 RotativeImage overlaps bordered label (#658)
    Signed-off-by: Faycal Abka <abka.faycal@gmail.com>

--- common/plugins/org.polarsys.kitalpha.common.sirius.extension.rotativeimage/src/org/polarsys/kitalpha/common/sirius/extension/rotativeimage/RotativeImageEditPartProvider.java
+++ common/plugins/org.polarsys.kitalpha.common.sirius.extension.rotativeimage/src/org/polarsys/kitalpha/common/sirius/extension/rotativeimage/RotativeImageEditPartProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 Thales Global Services S.A.S.
+ * Copyright (c) 2020, 2021 Thales Global Services S.A.S.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0
@@ -17,6 +17,8 @@ import org.eclipse.gmf.runtime.diagram.ui.services.editpart.AbstractEditPartProv
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.sirius.diagram.DNode;
 import org.eclipse.sirius.diagram.WorkspaceImage;
+import org.eclipse.sirius.diagram.ui.internal.edit.parts.DNode4EditPart;
+import org.eclipse.sirius.diagram.ui.internal.edit.parts.WorkspaceImageEditPart;
 import org.polarsys.kitalpha.common.sirius.extension.internal.rotativeimage.Activator;
 
 /**
@@ -28,20 +30,30 @@ public class RotativeImageEditPartProvider extends AbstractEditPartProvider {
 
 	@Override
 	protected Class<?> getNodeEditPartClass(final View view) {
+		String type = view.getType();
 		final EObject resolvedSemanticElement = ViewUtil.resolveSemanticElement(view);
-		if (resolvedSemanticElement instanceof WorkspaceImage) {
-			final WorkspaceImage customStyle = (WorkspaceImage) resolvedSemanticElement;
-			for (RotativeDescription desc : Activator.getDefault().getDescriptions()) {
-				if (customStyle.getWorkspacePath() != null && customStyle.getWorkspacePath().startsWith(desc.id))
-					return RotativeImageEditPart.class;
-			}
-		} else if (resolvedSemanticElement instanceof DNode) {
-			DNode spec = (DNode) resolvedSemanticElement;
-			if (spec.getOwnedStyle() != null && spec.getOwnedStyle() instanceof WorkspaceImage) {
-				WorkspaceImage customStyle = (WorkspaceImage) spec.getOwnedStyle();
+
+		if (String.valueOf(RotativeImageEditPart.VISUAL_ID).equals(type)) {
+			if (resolvedSemanticElement instanceof WorkspaceImage) {
+				final WorkspaceImage customStyle = (WorkspaceImage) resolvedSemanticElement;
 				for (RotativeDescription desc : Activator.getDefault().getDescriptions()) {
-					if (customStyle.getWorkspacePath() != null && customStyle.getWorkspacePath().startsWith(desc.id))
-						return RotativeDNode4EditPart.class;
+					if (customStyle.getWorkspacePath() != null 
+							&& customStyle.getWorkspacePath().startsWith(desc.id))
+						return RotativeImageEditPart.class;
+				}
+			}
+		} else {
+			if (String.valueOf(RotativeDNode4EditPart.VISUAL_ID).equals(type)) {
+				if (resolvedSemanticElement instanceof DNode) {
+					DNode spec = (DNode) resolvedSemanticElement;
+					if (spec.getOwnedStyle() != null && spec.getOwnedStyle() instanceof WorkspaceImage) {
+						WorkspaceImage customStyle = (WorkspaceImage) spec.getOwnedStyle();
+						for (RotativeDescription desc : Activator.getDefault().getDescriptions()) {
+							if (customStyle.getWorkspacePath() != null
+									&& customStyle.getWorkspacePath().startsWith(desc.id))
+								return RotativeDNode4EditPart.class;
+						}
+					}
 				}
 			}
 		}
