24fe2590d44bfa0026fb8016f9fb71671f81fd71
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2021-04-20 17:34:41 +0200

    [releng] Update Copyright years
    Change-Id: I4f80d4243ba211595227cc2412e62dd3f2a9b91a

--- Docgen/business/plugins/org.polarsys.kitalpha.doc.gen.business.core/src/org/polarsys/kitalpha/doc/gen/business/core/sirius/util/diagram/CoordinatesCalculator.java
+++ Docgen/business/plugins/org.polarsys.kitalpha.doc.gen.business.core/src/org/polarsys/kitalpha/doc/gen/business/core/sirius/util/diagram/CoordinatesCalculator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2020 Thales Global Services S.A.S.
+ * Copyright (c) 2014, 2021 Thales Global Services S.A.S.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0
