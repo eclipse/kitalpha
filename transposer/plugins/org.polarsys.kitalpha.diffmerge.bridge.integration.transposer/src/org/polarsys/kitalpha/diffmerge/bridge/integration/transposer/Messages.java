ba59964722a223f587a4eb25a03ba60ea1c5b3ed
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2021-09-09 11:32:14 +0200

    #688 OpenSource Diff/Merge Bridge Integration plugin
    Add Diff/Merge Bridge Integration plugin to the repo. Include it in the
    transposer feature.
    
    Setup branding using .properties files content
    
    Add to new transposer diffmerge feature
    
    Change-Id: I37e425fa67e2d79a4ec29401b4276b64fc6deb19

---
+++ transposer/plugins/org.polarsys.kitalpha.diffmerge.bridge.integration.transposer/src/org/polarsys/kitalpha/diffmerge/bridge/integration/transposer/Messages.java
@@ -0,0 +1,30 @@
+/*********************************************************************
+ * Copyright (c) 2014-2019 Thales Global Services S.A.S.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Thales Global Services S.A.S. - initial API and implementation
+ **********************************************************************/
+package org.polarsys.kitalpha.diffmerge.bridge.integration.transposer;
+
+import org.eclipse.osgi.util.NLS;
+
+@SuppressWarnings("javadoc")
+public class Messages extends NLS {
+  private static final String BUNDLE_NAME = "org.polarsys.kitalpha.diffmerge.bridge.integration.transposer.messages"; //$NON-NLS-1$
+  public static String bridgeLoggerTransposerRunStepMessage;
+  public static String bridgeLoggerTransposerBridgeTransformationKeyNullIllegalArgument;
+  public static String bridgeLoggerTransposerBridgeTransformationKeySourceObjectNullIllegalArgument;
+  public static String bridgeLoggerTransposerBridgeTransformationKeyRoleNullIllegalArgument;
+  static {
+    // initialize resource bundle
+    NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+  }
+
+  private Messages() {
+  }
+}
