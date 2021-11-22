7a943ee1aa3274de68c4f9600ca86d8a803a0253
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2021-11-22 17:43:47 +0100

    #703 Add SVG support in rotative images
    Add support for rotative SVG images.
    Both Rotative and 4Images modes are respectively supported in
    RotativeSVGWorkspaceImageFigure and
    Rotative4ImagesSVGWorkspaceImageFigure. Both are overridden
    versions of Sirius SVGWorkspaceImageFigure.
    The first one rely on a custom SimpleImageTranscoder
    (RotativeSVGImageTranscoder) to apply rotation on
    the image from the lone svg linked from the WorkspaceImage
    style description.
    The second one rely on four images (one for each side of the
    parent figure).
    
    Improved performances in Activator to retrieve RotativeDescription from
    extensions and store descriptions in a Map.
    
    Added JU tests for diagrams
    
    Update TP:
    - Add batik.transposer in the target platform for
    RotativeSVGImageTranscoder dependencies.
    - Add new version of Sirius including a linked evolution
      (https://bugs.eclipse.org/bugs/show_bug.cgi?id=577397)
    
    Cleanup unused code.
    
    Change-Id: I0000000000000000000000000000000000000000
    Signed-off-by: Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>

--- common/plugins/org.polarsys.kitalpha.common.sirius.extension.rotativeimage/src/org/polarsys/kitalpha/common/sirius/extension/rotativeimage/editpart/RotativeImageEditPartProvider.java
+++ common/plugins/org.polarsys.kitalpha.common.sirius.extension.rotativeimage/src/org/polarsys/kitalpha/common/sirius/extension/rotativeimage/editpart/RotativeImageEditPartProvider.java
@@ -9,7 +9,7 @@
  * Contributors:
  *   Thales Global Services S.A.S - initial API and implementation
  *******************************************************************************/
-package org.polarsys.kitalpha.common.sirius.extension.rotativeimage;
+package org.polarsys.kitalpha.common.sirius.extension.rotativeimage.editpart;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmf.runtime.diagram.core.util.ViewUtil;
@@ -19,12 +19,13 @@ import org.eclipse.sirius.diagram.DNode;
 import org.eclipse.sirius.diagram.WorkspaceImage;
 import org.eclipse.sirius.diagram.ui.internal.edit.parts.DNode4EditPart;
 import org.eclipse.sirius.diagram.ui.internal.edit.parts.WorkspaceImageEditPart;
-import org.polarsys.kitalpha.common.sirius.extension.internal.rotativeimage.Activator;
+import org.polarsys.kitalpha.common.sirius.extension.rotativeimage.Activator;
+import org.polarsys.kitalpha.common.sirius.extension.rotativeimage.extension.RotativeDescription;
 
 /**
  * Specific Edit Part Provider for rotative image
  * 
- * @author ymortier,pdul
+ * @author ymortier,pdul,adieumegard
  */
 public class RotativeImageEditPartProvider extends AbstractEditPartProvider {
 
@@ -34,26 +35,42 @@ public class RotativeImageEditPartProvider extends AbstractEditPartProvider {
 		final EObject resolvedSemanticElement = ViewUtil.resolveSemanticElement(view);
 
 		if (String.valueOf(WorkspaceImageEditPart.VISUAL_ID).equals(type) && resolvedSemanticElement instanceof WorkspaceImage) {
-			final WorkspaceImage customStyle = (WorkspaceImage) resolvedSemanticElement;
-			for (RotativeDescription desc : Activator.getDefault().getDescriptions()) {
-				if (customStyle.getWorkspacePath() != null 
-						&& customStyle.getWorkspacePath().startsWith(desc.id))
-					return RotativeImageEditPart.class;
+			if (isRotativeImageEditPart(resolvedSemanticElement)) {
+				return RotativeImageEditPart.class;
 			}
 		} else {
 			if (String.valueOf(DNode4EditPart.VISUAL_ID).equals(type) && resolvedSemanticElement instanceof DNode) {
-				DNode spec = (DNode) resolvedSemanticElement;
-				if (spec.getOwnedStyle() != null && spec.getOwnedStyle() instanceof WorkspaceImage) {
-					WorkspaceImage customStyle = (WorkspaceImage) spec.getOwnedStyle();
-					for (RotativeDescription desc : Activator.getDefault().getDescriptions()) {
-						if (customStyle.getWorkspacePath() != null
-								&& customStyle.getWorkspacePath().startsWith(desc.id))
-							return RotativeDNode4EditPart.class;
-					}
+				if (isRotativeDNode4editPart(resolvedSemanticElement)) {
+					return RotativeDNode4EditPart.class;
 				}
 			}
 		}
 		return super.getNodeEditPartClass(view);
+	}
+
+	private boolean isRotativeDNode4editPart(final EObject resolvedSemanticElement) {
+		DNode spec = (DNode) resolvedSemanticElement;
+		if (spec.getOwnedStyle() instanceof WorkspaceImage) {
+			WorkspaceImage customStyle = (WorkspaceImage) spec.getOwnedStyle();
+			if (customStyle.getWorkspacePath() != null) {
+			    RotativeDescription rotativeDescription = Activator.getDefault().getDescriptions().get(customStyle.getWorkspacePath());
+			    if (rotativeDescription != null) {
+		            return true;
+		        }
+			}
+		}
+		return false;
+	}
+
+	private boolean isRotativeImageEditPart(final EObject resolvedSemanticElement) {
+		final WorkspaceImage customStyle = (WorkspaceImage) resolvedSemanticElement;
+		if (customStyle.getWorkspacePath() != null) {
+		    RotativeDescription rotativeDescription = Activator.getDefault().getDescriptions().get(customStyle.getWorkspacePath());
+		    if (rotativeDescription != null) {
+		        return true;
+		    }
+		}
+		return false;
 	}
 
 }
