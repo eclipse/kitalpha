56f5cd31dc641ee8e3553baac1629d3b35c44907
Laurent Fasani <laurent.fasani@obeo.fr>
2021-11-30 14:24:45 +0100

    #701 Adapt "Add link" wizard to warn about absolute and relative file
    Absolute and project relative file options are now no recommended if the
    user wants to share information and collaborate.
    
    Change-Id: I4e4f2183a1cda5f0a6ed0f93be6fa66d331c391a
    Signed-off-by: Laurent Fasani <laurent.fasani@obeo.fr>

--- richtext/plugins/org.polarsys.kitalpha.richtext.widget.tools/src/org/polarsys/kitalpha/richtext/widget/tools/dialogs/MDEAddLinkDialog.java
+++ richtext/plugins/org.polarsys.kitalpha.richtext.widget.tools/src/org/polarsys/kitalpha/richtext/widget/tools/dialogs/MDEAddLinkDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2020 Thales Global Services S.A.S.
+ * Copyright (c) 2017, 2021 Thales Global Services S.A.S.
  *  This program and the accompanying materials are made available under the
  *  terms of the Eclipse Public License 2.0 which is available at
  *  http://www.eclipse.org/legal/epl-2.0
@@ -13,12 +13,14 @@ package org.polarsys.kitalpha.richtext.widget.tools.dialogs;
 
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -36,168 +38,210 @@ import org.polarsys.kitalpha.richtext.widget.tools.utils.Constants;
 import org.polarsys.kitalpha.richtext.widget.tools.utils.Tuple;
 
 public class MDEAddLinkDialog extends MDEOkCancelDialog {
-	
-	protected MDERichTextWidget richText;
 
-	protected Text urlText;
+  protected MDERichTextWidget richText;
 
-	protected String basePath;
+  protected Text urlText;
 
-	protected Composite composite;
+  protected String basePath;
 
-	protected Label urlLabel;
+  protected Composite composite;
 
-	protected Text urlDisplayNameText;
+  protected Label urlLabel;
 
-	protected Label urlDisplayNameLabel;
+  protected Text urlDisplayNameText;
 
-	protected Combo linkTypeCombo;
+  protected Label urlDisplayNameLabel;
 
-	protected Button browseButton;
+  protected Combo linkTypeCombo;
 
-	protected String linkType;
-	
-	protected LinkManager linkManager;
+  protected Button browseButton;
 
+  protected String linkType;
 
-	public MDEAddLinkDialog(Shell parentShell, MDERichTextWidget richText, LinkManager linkManager) {
-		super(parentShell);
-		this.linkManager = linkManager;
-		this.richText = richText;
-		this.basePath = MDERichTextHelper.getProjectPath(richText.getElement());
-	}
+  protected LinkManager linkManager;
 
-	@Override
-	protected Control createDialogArea(Composite parent) {
-		
-		Composite composite = (Composite) super.createDialogArea(parent);
-		GridLayout gridLayout = (GridLayout) composite.getLayout();
-		gridLayout.numColumns = 3;
-		
-		createUrlLabel(composite);
-		createDisplayTextURL(composite);
-		createListLinksType(composite);
-		createBrowserButton(composite);
+  protected Composite informationArea;
 
-		String selectedText = richText.getSelectedText();
-		if (selectedText != null){
-			urlDisplayNameText.setText(selectedText);
-		}
+  private Label messageImageLabel1;
 
-		super.getShell().setText(Messages.RichTextWidget_Dialog_Add_Link);
+  private Label messageLabel1;
 
-		return composite;
-	}
+  public MDEAddLinkDialog(Shell parentShell, MDERichTextWidget richText, LinkManager linkManager) {
+    super(parentShell);
+    this.linkManager = linkManager;
+    this.richText = richText;
+    this.basePath = MDERichTextHelper.getProjectPath(richText.getElement());
+  }
 
-	private void createBrowserButton(Composite composite) {
-		browseButton = new Button(composite, SWT.PUSH);
-		browseButton.setLayoutData(new GridData());
-		browseButton.setText(Messages.RichTextWidget_Dialog_Add_Link_Browse);
-		browseButton.setEnabled(false);
-		browseButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent event) {
-				Tuple<String, String> path = null;
-				path = linkManager.getURI(linkType, basePath, richText.getElement());
-				
-				if (path != null){
-					String path2Object = path.getFirst();
-					String objectLabel = path.getSecond();
-					if (path2Object != null){
+  @Override
+  protected Control createDialogArea(Composite parent) {
+
+    Composite mainComposite = (Composite) super.createDialogArea(parent);
+    mainComposite.setLayout(new GridLayout());
+
+    Composite composite = new Composite(mainComposite, SWT.NONE);
+    composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+    GridLayout gridLayout = new GridLayout();
+    gridLayout.numColumns = 3;
+    composite.setLayout(gridLayout);
+
+    createUrlLabel(composite);
+    createDisplayTextURL(composite);
+    createListLinksType(composite);
+    createBrowserButton(composite);
+
+    String selectedText = richText.getSelectedText();
+    if (selectedText != null) {
+      urlDisplayNameText.setText(selectedText);
+    }
+
+    createMessagePart(mainComposite);
+
+    super.getShell().setText(Messages.RichTextWidget_Dialog_Add_Link);
+
+    return composite;
+  }
+
+  protected void createMessagePart(Composite parent) {
+    Composite composite = new Composite(parent, SWT.NONE);
+    composite.setLayout(new GridLayout(2, false));
+
+    messageImageLabel1 = new Label(composite, SWT.NONE);
+    messageImageLabel1.setImage(JFaceResources.getImage(DLG_IMG_MESSAGE_WARNING));
+    messageLabel1 = new Label(composite, SWT.WRAP);
+    messageLabel1
+        .setText("Choice not recommended if you want to share information and collaborate.\nUse URL link instead.");
+
+    updateHeaderMessages();
+  }
+
+  private void updateHeaderMessages() {
+
+    String selectedItem = linkTypeCombo.getItem(linkTypeCombo.getSelectionIndex());
+    boolean displayWarning = Constants.FILE_LABEL.equals(selectedItem)
+        || Constants.FILE_LOCAL_LABEL.equals(selectedItem);
+    messageImageLabel1.setVisible(displayWarning);
+    messageLabel1.setVisible(displayWarning);
+
+    this.getShell().layout(true, true);
+  }
+
+  private void createBrowserButton(Composite composite) {
+    browseButton = new Button(composite, SWT.PUSH);
+    browseButton.setLayoutData(new GridData());
+    browseButton.setText(Messages.RichTextWidget_Dialog_Add_Link_Browse);
+    browseButton.setEnabled(false);
+    browseButton.addSelectionListener(new SelectionAdapter() {
+      @Override
+      public void widgetSelected(SelectionEvent event) {
+        Tuple<String, String> path = null;
+        path = linkManager.getURI(linkType, basePath, richText.getElement());
+
+        if (path != null) {
+          String path2Object = path.getFirst();
+          String objectLabel = path.getSecond();
+          if (path2Object != null) {
             if (Constants.FILE_LABEL.equals(linkType)) {
               path2Object = path2Object.replace("\\", "/");
             }
-						urlText.setText(path2Object);
-					}
-					urlDisplayNameText.setText(objectLabel != null? objectLabel: "");
-				}
-			}
-		});
-		browseButton.moveAbove(urlDisplayNameLabel);
-	}
+            urlText.setText(path2Object);
+          }
+          urlDisplayNameText.setText(objectLabel != null ? objectLabel : "");
+        }
+      }
+    });
+    browseButton.moveAbove(urlDisplayNameLabel);
+  }
 
-	private void createListLinksType(Composite composite) {
-		linkTypeCombo = new Combo(composite, SWT.BORDER | SWT.READ_ONLY);
-		GridData comboGridData = new GridData(GridData.FILL_HORIZONTAL);
-		comboGridData.horizontalSpan = 2;
-		linkTypeCombo.setLayoutData(comboGridData);
-		
-		final String[] itemsLinkList = linkManager.getAllLinkLabels().toArray(new String[0]);
-		
-		linkTypeCombo.setItems(itemsLinkList);
-		linkType = itemsLinkList[0];
-		linkTypeCombo.setText(linkType);
-		linkTypeCombo.addSelectionListener(new SelectionListener() {
-			@SuppressWarnings("synthetic-access")
-			public void widgetSelected(SelectionEvent event) {
-				linkType = itemsLinkList[linkTypeCombo.getSelectionIndex()];
-				browseButton.setEnabled(!Constants.URL_LABEL.equals(linkType));
-				urlLabel.setEnabled(Constants.URL_LABEL.equals(linkType) || Constants.FILE_LABEL.equals(linkType) || Constants.FILE_LOCAL_LABEL.equals(linkType));
-				urlText.setEnabled(Constants.URL_LABEL.equals(linkType) ||  Constants.FILE_LABEL.equals(linkType) || Constants.FILE_LOCAL_LABEL.equals(linkType));
-				if (Constants.URL_LABEL.equals(linkType)) {
-					urlDisplayNameText.setText(richText.getSelectedText());
-				} else {
-					urlDisplayNameText.setText(""); //$NON-NLS-1$
-				}
-				urlText.setText(""); //$NON-NLS-1$
-			}
+  private void createListLinksType(Composite composite) {
+    linkTypeCombo = new Combo(composite, SWT.BORDER | SWT.READ_ONLY);
+    GridData comboGridData = new GridData(GridData.FILL_HORIZONTAL);
+    comboGridData.horizontalSpan = 2;
+    linkTypeCombo.setLayoutData(comboGridData);
 
-			public void widgetDefaultSelected(SelectionEvent event) {
-				// do nothing
-			}
-		});
-		
-		linkTypeCombo.moveAbove(urlLabel);
-	}
+    final String[] itemsLinkList = linkManager.getAllLinkLabels().toArray(new String[0]);
 
-	private void createDisplayTextURL(Composite composite) {
-		
-		urlDisplayNameLabel = new Label(composite, SWT.NONE);
-		urlDisplayNameLabel.setText(Messages.RichTextWidget_Dialog_Add_Link_URL_Display);
-		
-		urlDisplayNameText = new Text(composite, SWT.BORDER);
-		GridDataFactory.fillDefaults().grab(true, false).hint(300, SWT.DEFAULT).applyTo(urlDisplayNameText);
+    linkTypeCombo.setItems(itemsLinkList);
+    linkType = itemsLinkList[0];
+    linkTypeCombo.setText(linkType);
+    linkTypeCombo.addSelectionListener(new SelectionListener() {
+      @Override
+      @SuppressWarnings("synthetic-access")
+      public void widgetSelected(SelectionEvent event) {
+        linkType = itemsLinkList[linkTypeCombo.getSelectionIndex()];
+        browseButton.setEnabled(!Constants.URL_LABEL.equals(linkType));
+        urlLabel.setEnabled(Constants.URL_LABEL.equals(linkType) || Constants.FILE_LABEL.equals(linkType)
+            || Constants.FILE_LOCAL_LABEL.equals(linkType));
+        urlText.setEnabled(Constants.URL_LABEL.equals(linkType) || Constants.FILE_LABEL.equals(linkType)
+            || Constants.FILE_LOCAL_LABEL.equals(linkType));
+        if (Constants.URL_LABEL.equals(linkType)) {
+          urlDisplayNameText.setText(richText.getSelectedText());
+        } else {
+          urlDisplayNameText.setText(""); //$NON-NLS-1$
+        }
+        urlText.setText(""); //$NON-NLS-1$
+        updateHeaderMessages();
+      }
 
-		Label linkTypeLabel = new Label(composite, SWT.NONE);
-		linkTypeLabel.setText(Messages.RichTextWidget_Dialog_Add_Link);
-		linkTypeLabel.moveAbove(urlLabel);
-	}
+      @Override
+      public void widgetDefaultSelected(SelectionEvent event) {
+        // do nothing
+      }
+    });
 
-	private void createUrlLabel(Composite composite) {
-		urlLabel = new Label(composite, SWT.NONE);
-		urlLabel.setText("URL");
-		urlText = new Text(composite, SWT.BORDER);
-		GridDataFactory.fillDefaults().grab(true, false).hint(300, SWT.DEFAULT).applyTo(urlText);
-		urlText.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				if (getOK() != null) {
-					getOK().setEnabled(urlText.getText().trim().length() > 0);
-				}
-			}
-		});
-	}
+    linkTypeCombo.moveAbove(urlLabel);
+  }
 
-	@Override
-	protected void createButtonsForButtonBar(Composite parent) {
-		createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
-		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
-	}
-	
-	@Override
-	protected void okPressed() {
-		String url = urlText.getText();
-		if (url != null && url.length() > 0) {
-			String urlDisplayName = urlDisplayNameText.getText();
-			if (urlDisplayName.trim().length() == 0) {
-				urlDisplayName = url;
-			}
-			String encodedURL = linkManager.encode(linkType, url, urlDisplayName);
-			richText.insertRawText(encodedURL);
-		}
+  private void createDisplayTextURL(Composite composite) {
 
-		urlText.setText(""); //$NON-NLS-1$
-		super.okPressed();
-		richText.forceFocus();
-	}
+    urlDisplayNameLabel = new Label(composite, SWT.NONE);
+    urlDisplayNameLabel.setText(Messages.RichTextWidget_Dialog_Add_Link_URL_Display);
+
+    urlDisplayNameText = new Text(composite, SWT.BORDER);
+    GridDataFactory.fillDefaults().grab(true, false).hint(300, SWT.DEFAULT).applyTo(urlDisplayNameText);
+
+    Label linkTypeLabel = new Label(composite, SWT.NONE);
+    linkTypeLabel.setText(Messages.RichTextWidget_Dialog_Add_Link);
+    linkTypeLabel.moveAbove(urlLabel);
+  }
+
+  private void createUrlLabel(Composite composite) {
+    urlLabel = new Label(composite, SWT.NONE);
+    urlLabel.setText("URL");
+    urlText = new Text(composite, SWT.BORDER);
+    GridDataFactory.fillDefaults().grab(true, false).hint(300, SWT.DEFAULT).applyTo(urlText);
+    urlText.addModifyListener(new ModifyListener() {
+      @Override
+      public void modifyText(ModifyEvent e) {
+        if (getOK() != null) {
+          getOK().setEnabled(urlText.getText().trim().length() > 0);
+        }
+      }
+    });
+  }
+
+  @Override
+  protected void createButtonsForButtonBar(Composite parent) {
+    createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+    createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
+  }
+
+  @Override
+  protected void okPressed() {
+    String url = urlText.getText();
+    if (url != null && url.length() > 0) {
+      String urlDisplayName = urlDisplayNameText.getText();
+      if (urlDisplayName.trim().length() == 0) {
+        urlDisplayName = url;
+      }
+      String encodedURL = linkManager.encode(linkType, url, urlDisplayName);
+      richText.insertRawText(encodedURL);
+    }
+
+    urlText.setText(""); //$NON-NLS-1$
+    super.okPressed();
+    richText.forceFocus();
+  }
 
 }
