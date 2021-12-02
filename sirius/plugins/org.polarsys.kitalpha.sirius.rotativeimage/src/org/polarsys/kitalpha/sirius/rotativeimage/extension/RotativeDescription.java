e6812079b8c39a3bf111f056d56df537b9cf3f1a
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2021-12-03 00:55:43 +0100

    #707 Rename and move rotative image plugin
    Renamed org.polarsys.kitalpha.common.sirius.extension.rotativeimage
    to org.polarsys.kitalpha.sirius.rotativeimage
    Moved plugins from common folder to sirius folder
    Created new feature org.polarsys.kitalpha.sirius.rotativeimage.feature
    Added new feature to runtimecore update site and thus to runtime and sdk
    ones. Added feature in product.
    
    Improved image retrieval and comments in RotativeImageDisplayTest
    
    Change-Id: I0000000000000000000000000000000000000000
    Signed-off-by: Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>

---
+++ sirius/plugins/org.polarsys.kitalpha.sirius.rotativeimage/src/org/polarsys/kitalpha/sirius/rotativeimage/extension/RotativeDescription.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2020, 2021 Thales Global Services S.A.S.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *  
+ * Contributors:
+ *   Thales Global Services S.A.S - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.kitalpha.sirius.rotativeimage.extension;
+
+public class RotativeDescription {
+	
+	public static final int FOUR_IMAGES = 0;
+	public static  final int ROTATIVE = 1;
+	
+	public String id;
+	public int mode;
+	
+	public RotativeDescription(String id, int mode) {
+		this.id=id;
+		this.mode=mode;
+	}
+}
