78726fd2f8303de43895b724c30b586ed5be0de3
Erwann Traisnel <erwann.traisnel@obeo.fr>
2023-10-09 16:25:24 +0200

    #779 Fix Transposer Error log
    Change-Id: I78ed573d91375a49e641c669ffa4e1b4e7feeffe
    Signed-off-by: Erwann Traisnel <erwann.traisnel@obeo.fr>

--- transposer/plugins/org.polarsys.kitalpha.transposer.rules.handler/src/org/polarsys/kitalpha/transposer/rules/handler/rules/contribution/spec/ContributedPurposeSpec.java
+++ transposer/plugins/org.polarsys.kitalpha.transposer.rules.handler/src/org/polarsys/kitalpha/transposer/rules/handler/rules/contribution/spec/ContributedPurposeSpec.java
@@ -10,14 +10,12 @@
  *    Thales Global Services S.A.S - initial API and implementation
  *******************************************************************************/
 
-
 package org.polarsys.kitalpha.transposer.rules.handler.rules.contribution.spec;
 
 import java.util.Collection;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-
 import org.polarsys.kitalpha.transposer.rules.handler.RuleHandlerPlugin;
 import org.polarsys.kitalpha.transposer.rules.handler.rules.common.Mapping;
 import org.polarsys.kitalpha.transposer.rules.handler.rules.contribution.impl.ContributedPurposeImpl;
@@ -39,8 +37,8 @@ public class ContributedPurposeSpec extends ContributedPurposeImpl {
   @Override
   public Mapping getMostGenericMapping() {
     if (_mostGenericMapping == null) {
-		computeMostGenericMapping();
-	}
+      computeMostGenericMapping();
+    }
 
     return _mostGenericMapping;
   }
@@ -53,8 +51,6 @@ public class ContributedPurposeSpec extends ContributedPurposeImpl {
     for (Mapping currentMapping : getMappings()) {
       if (currentMapping.getExtendedMapping() == null || !getMappings().contains(currentMapping.getExtendedMapping())) {
         if (lowestMapping != null) {
-          System.out
-              .println("ERROR => PROHIBITED SITUATION : There are many specifics mappings for the purpose <" + currentMapping.getPurpose().getName() + ">"); //$NON-NLS-1$ //$NON-NLS-2$
           break;
         }
         lowestMapping = currentMapping;
@@ -69,24 +65,50 @@ public class ContributedPurposeSpec extends ContributedPurposeImpl {
   @Override
   public IStatus validate() {
     int severity = IStatus.INFO;
-    String message = "Contributed purpose " + getName() + " is valid, check the error log for information."; //$NON-NLS-1$ //$NON-NLS-2$
+    String message = "Contributed purpose " + getName() + " is valid."; //$NON-NLS-1$ //$NON-NLS-2$
 
+    IStatus multipleMappingsStatus = validateMultipleMappingsForPurposes();
+    boolean hasMultipleMappings = multipleMappingsStatus.getSeverity() == IStatus.WARNING;
+    
+    if (hasMultipleMappings)  severity = multipleMappingsStatus.getSeverity();
+    
     for (Mapping mapping : getMappings()) {
       IStatus result = mapping.validate();
       if (result.getSeverity() != IStatus.INFO) {
-		severity = result.getSeverity();
-	}
+        severity = result.getSeverity();
+      }
     }
+    
+   
 
     if (severity == IStatus.WARNING) {
-      message = "WARNING ! Contributed purpose " + getName() + " validates with warnings, check the error log for information."; //$NON-NLS-1$ //$NON-NLS-2$
+      message = "WARNING ! Contributed purpose " + getName() //$NON-NLS-1$
+          + " validates with warnings, check the error log for information."; //$NON-NLS-1$
     }
 
-    if (severity == IStatus.WARNING) {
+    if (severity == IStatus.ERROR) {
       message = "ERROR ! Contributed purpose " + getName() + " is invalid, check the error log for information."; //$NON-NLS-1$ //$NON-NLS-2$
+    }
+    
+    if (hasMultipleMappings) {
+      message += "\n" + multipleMappingsStatus.getMessage();//$NON-NLS-1$
     }
 
     return new Status(severity, RuleHandlerPlugin.PLUGIN_ID, message);
+  }
+  
+  private IStatus validateMultipleMappingsForPurposes() {
+    Mapping lowestMapping = null;
+    for (Mapping currentMapping : getMappings()) {
+      if (currentMapping.getExtendedMapping() == null || !getMappings().contains(currentMapping.getExtendedMapping())) {
+        if (lowestMapping != null) {
+          return new Status(IStatus.WARNING,RuleHandlerPlugin.PLUGIN_ID, 
+              "WARNING => PROHIBITED SITUATION : There are many specifics mappings for the purpose <" + currentMapping.getPurpose().getName() + ">"); //$NON-NLS-1$ //$NON-NLS-2$)
+        }
+        lowestMapping = currentMapping;
+      }
+    }
+    return new Status(IStatus.OK,RuleHandlerPlugin.PLUGIN_ID, "" );
   }
 
   /**
@@ -103,18 +125,17 @@ public class ContributedPurposeSpec extends ContributedPurposeImpl {
     }
     return null;
   }
-  
-  
-  @Override
-	public boolean isPrivate() {
-		Collection<Mapping> mappings = Collections2.filter(getMappings(), new Predicate<Mapping>() {
 
-			@Override
-			public boolean apply(Mapping mapping) {
-				return !mapping.isPrivate();
-			}
-		});
-		
-		return mappings.isEmpty();
-	}
+  @Override
+  public boolean isPrivate() {
+    Collection<Mapping> mappings = Collections2.filter(getMappings(), new Predicate<Mapping>() {
+
+      @Override
+      public boolean apply(Mapping mapping) {
+        return !mapping.isPrivate();
+      }
+    });
+
+    return mappings.isEmpty();
+  }
 }
