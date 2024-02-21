1cffa690051c9e758e6c86ad0bc9a59cb2c04468
Erwann Traisnel <erwann.traisnel@obeo.fr>
2024-02-21 14:11:44 +0100

    #785 Docgen encoding set to UTF-8
    UTF-8 instead of ISO-8859-1
    
    Change-Id: I3a2617f5691cf3944ae68b27c388d3357f37fce3
    Signed-off-by: Erwann Traisnel <erwann.traisnel@obeo.fr>

--- Docgen/business/plugins/org.polarsys.kitalpha.doc.gen.business.core/src/org/polarsys/kitalpha/doc/gen/business/core/task/IndexingConceptsTask.java
+++ Docgen/business/plugins/org.polarsys.kitalpha.doc.gen.business.core/src/org/polarsys/kitalpha/doc/gen/business/core/task/IndexingConceptsTask.java
@@ -87,14 +87,14 @@ public class IndexingConceptsTask implements ITaskProduction {
 	private static Matcher mListStartEnd = pListStartEnd.matcher(""); //for lists
 
 
-	private static final String HEADER = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>"
+	private static final String HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
 			+ "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\""
 			+ "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">"
 			+ "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">"
 			+
 
 			"<head>"
-			+ "<meta http-equiv=\"content-type\" content=\"text/html;charset=ISO-8859-1\" />"
+			+ "<meta http-equiv=\"content-type\" content=\"text/html;charset=UTF-8\" />"
 			+ "<meta http-equiv=\"Content-Style-Type\" content=\"text/css\" />"
 			+
 
